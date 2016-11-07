8ce7db11ea1ffb0bd4dfc4e522a3ebdfd540492a
nverno <noah.v.peart@gmail.com>
2016-11-07 17:16:11 -0500

    squash

--- test/nvp-tests.el
+++ test/nvp-tests.el
@@ -1,13 +1,20 @@
 (require 'nvp)
 (require 'ert)
 
-(defmacro nvp--should- (from to)
+(defmacro nvp--should (from to)
   `(with-temp-buffer
      (let ()
        (insert ,from)
        ;; (indent-region (point-min) (point-max))
        (should (string= (buffer-substring-no-properties (point-min) (point-max))
                       ,to)))))
+
+(ert-deftest nvp--test-program ()
+  "os dependent program expansion"
+  (let ((nvp-g++-program "c:/g++"))
+    (if (eq system-type 'windows-nt)
+        (should (eq nvp-g++-program (nvp-program "g++")))
+      (should (string= "g++" (nvp-program "g++"))))))
 
 (defun nvp--run-tests ()
   (interactive)

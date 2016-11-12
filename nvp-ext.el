fbfe7f00e72210535723520e2780c0f62d994d2b
nverno <noah.v.peart@gmail.com>
2016-11-11 19:00:39 -0500

    squash

---
+++ nvp-ext.el
@@ -0,0 +1,47 @@
+;;; nvp-ext --- 
+
+;; This is free and unencumbered software released into the public domain.
+
+;; Author: Noah Peart <noah.v.peart@gmail.com>
+;; URL: https://github.com/nverno/
+;; Package-Requires: 
+;; Created: 11 November 2016
+
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;; Code:
+(eval-when-compile
+  (require 'nvp-macro))
+
+(nvp-with-gnu
+  ;; do sudo command and return process object
+  (defun nvp-ext-sudo-command (password &optional command buffer)
+    (interactive (list (read-passwd "Password: ")))
+    (let* ((cmd (or command (read-shell-command "Command: ")))
+           (proc (start-process-shell-command
+                  "bash" (or buffer "*nvp-install*")
+                  (concat "sudo bash " cmd))))
+      (process-send-string proc password)
+      (process-send-string proc "\r")
+      (process-send-eof proc)
+      proc)))
+
+
+(provide 'nvp-ext)
+;;; nvp-ext.el ends here

c0be4685bccb909d43fa33abe81c0555d40784ae
nverno <noah.v.peart@gmail.com>
2016-11-12 04:13:34 -0500

    squash

--- nvp-env.el
+++ nvp-env.el
@@ -1,4 +1,4 @@
-;;; nvp-path --- 
+;;; nvp-env --- 
 
 ;; This is free and unencumbered software released into the public domain.
 
@@ -27,29 +27,29 @@
 ;;; Commentary:
 ;;; Code:
 (eval-when-compile
+  (require 'nvp-macro)
   (require 'cl-lib))
 (require 'nvp)
 (require 'nvp-log)
 
 ;; ------------------------------------------------------------
-
-;;; Environment Variables
+;;; PATH
 
 ;; Append DIR to environment variable PATH and EXEC-PATH.
 ;;;###autoload
-(defun nvp-path-exec-add (dir)
+(defun nvp-env-exec-add (dir)
   (let ((path (cl-remove-duplicates
                (cons dir (split-string (getenv "PATH") path-separator))
                :test (lambda (x y)
-                       (if (eq system-type 'windows-nt)
-                           (string= (downcase x) (downcase y))
-                         (string= x y))))))
+                       (nvp-with-gnu/w32
+                           (string= x y)
+                         (string= (downcase x) (downcase y)))))))
     (setenv "PATH" (mapconcat 'identity path path-separator))
     (setq exec-path path)))
 
 ;; move path entries matching regex to front of path and return
 ;; new path, preserve existing order of entries otherwise
-(defun nvp-path-rearrange-path (regex &optional case-fold)
+(defun nvp-env-rearrange-path (regex &optional case-fold)
   (let ((parts (split-string (getenv "PATH") path-separator))
         (case-fold-search case-fold)
         hits misses)
@@ -62,60 +62,64 @@
 
 ;; rearrange process environmen path and return process-environment
 ;; with new value tacked onto front (first gets used)
-(defun nvp-path-rearrange-process-path (regex &optional case-fold)
+(defun nvp-env-rearrange-process-path (regex &optional case-fold)
   (cons (concat "PATH="
                 (mapconcat 'identity
-                           (nvp-path-rearrange-path regex case-fold)
+                           (nvp-env-rearrange-path regex case-fold)
                            path-separator))
         process-environment))
 
-;; update registry value on windows
-(defun nvp-path-w32-setenv! (env-var value &optional exec clobber)
-  (let* ((ps (expand-file-name "tools/Set-Env.ps1" nvp--dir))
-         (val (replace-regexp-in-string "/" "\\\\" value)))
-    (w32-shell-execute "runas" "powershell"
-                       (format " -File %s \"%s\" \"%s\" %s"
-                               ps env-var val (if clobber "-Clobber" "")))
-    ;; update env for current session as well
-    (when exec
-      (if (string= (upcase env-var) "PATH")
-          (nvp-path-exec-add val)
-        (setenv env-var val)))))
+;; ------------------------------------------------------------
+;;; Setenv
 
-;; Update registry value ENV-VAR by setting/appending VALUE (user).
-;; add to exec-path if EXEC is non-nil. If CLOBBER is non-nil,
-;; overwrite variable if it already exists.
-;;;###autoload
-(defun nvp-path-setenv! (env-var value &optional exec clobber)
-  (cond
-   ((eq system-type 'windows-nt)
-    (nvp-path-w32-setenv! env-var value exec clobber))
-   (t (user-error "FIXME"))))
+;;;###autoload (autoload 'nvp-env-setenv! "nvp-env")
+(nvp-with-w32
+  ;; Update registry value ENV-VAR by setting/appending VALUE (user).
+  ;; add to exec-path if EXEC is non-nil. If CLOBBER is non-nil,
+  ;; overwrite variable if it already exists.
+  (defun nvp-env-setenv! (env-var value &optional exec clobber)
+    (let* ((ps (expand-file-name "tools/Set-Env.ps1" nvp--dir))
+           (val (replace-regexp-in-string "/" "\\\\" value)))
+      (w32-shell-execute "runas" "powershell"
+                         (format " -File %s \"%s\" \"%s\" %s"
+                                 ps env-var val (if clobber "-Clobber" "")))
+      ;; update env for current session as well
+      (when exec
+        (if (string= (upcase env-var) "PATH")
+            (nvp-env-exec-add val)
+          (setenv env-var val))))))
 
+(nvp-with-gnu
+  (defun nvp-env-setenv! (env-var value &optional exec clobber)
+    ;; FIXME: todo
+    (user-error "TODO")))
+
+;; ------------------------------------------------------------
 ;;; Search Path
 
-;; update executable database, rerun build script
-;; defaults to $APPDATA/exe-index on windows
-;;;###autoload
-(defun nvp-path-update-path-db (&optional path)
-  (nvp-log "Updating executable database")
-  (cond
-   ((eq system-type 'windows-nt)
-    (set-process-sentinel
-     (apply #'start-process
-            "updatedb" nvp-log-buffer "powershell"
-            `("-f" ,(expand-file-name "Update-ExecDB.ps1" nvp/binw)
-              ,@path))
-     #'nvp-path-update-path-db-sentinel))))
+;;;###autoload (autoload 'nvp-env-update-path-db "nvp-env")
+(nvp-with-w32
+  ;; update executable database, rerun build script
+  ;; defaults to $APPDATA/exe-index on windows
+  (defun nvp-env-update-path-db (&optional path)
+    (nvp-log "Updating executable database")
+    (cond
+     ((eq system-type 'windows-nt)
+      (set-process-sentinel
+       (apply #'start-process
+              "updatedb" nvp-log-buffer "powershell"
+              `("-f" ,(expand-file-name "Update-ExecDB.ps1" nvp/binw)
+                ,@path))
+       #'nvp-env-update-path-db-sentinel))))
 
-(defun nvp-path-update-path-db-sentinel (p m)
-  (nvp-log (format "%s: %s" (process-name p) m))
-  (when (zerop (process-exit-status p))
-    (nvp-log "DB Update completed")))
+  (defun nvp-env-update-path-db-sentinel (p m)
+    (nvp-log (format "%s: %s" (process-name p) m))
+    (when (zerop (process-exit-status p))
+      (nvp-log "DB Update completed"))))
 
 ;; ------------------------------------------------------------
 
 (declare-function w32-shell-execute "w32")
 
-(provide 'nvp-path)
-;;; nvp-path.el ends here
+(provide 'nvp-env)
+;;; nvp-env.el ends here

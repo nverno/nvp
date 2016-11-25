49b9d6539f7407ca0b248eaa261a970b3779d38c
nverno <noah.v.peart@gmail.com>
2016-11-24 22:54:40 -0500

    squash

---
+++ nvp-buffer.el
@@ -0,0 +1,156 @@
+;;; nvp-buffer --- 
+
+;; This is free and unencumbered software released into the public domain.
+
+;; Author: Noah Peart <noah.v.peart@gmail.com>
+;; URL: https://github.com/nverno/nvp
+;; Package-Requires: 
+;; Created: 24 November 2016
+
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;; Code:
+(eval-when-compile
+  (require 'nvp-macro)
+  (require 'cl-lib)
+  (require 's))
+
+;;;###autoload
+(defun nvp-buffer-kill-other-buffers () 
+  (interactive)
+    (mapc #'kill-buffer (cdr (buffer-list (current-buffer)))))
+
+;; Transpose the buffers shown in two windows.
+;;;###autoload
+(defun nvp-buffer-transpose-windows (arg)
+  (interactive "p")
+  (let ((selector (if (>= arg 0) 'next-window 'previous-window)))
+    (while (/= arg 0)
+      (let ((this-win (window-buffer))
+	    (next-win (window-buffer (funcall selector))))
+	(set-window-buffer (selected-window) next-win)
+	(set-window-buffer (funcall selector) this-win)
+	(select-window (funcall selector)))
+      (setq arg (if (cl-plusp arg) (1- arg) (1+ arg))))))
+
+;;;###autoload
+(defun nvp-buffer-swap-windows ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win (window-buffer))
+             (next-win (window-buffer (next-window)))
+             (this-edge (window-edges (selected-window)))
+             (next-edge (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-edge)
+                                         (car next-edge))
+                                     (<= (cadr this-edge)
+                                         (cadr next-edge)))))
+             (splitter
+              (if (= (car this-edge)
+                     (car (window-edges (next-window))))
+                  #'split-window-horizontally
+                #'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win)
+          (set-window-buffer (next-window) next-win)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+
+;; Return list of buffers where `major-mode' matches MODE.
+;;;###autoload
+(defun nvp-buffer-matching-mode (mode)
+  (let ((buffer-mode-matches '()))
+    (dolist (buf (buffer-list))
+      (with-current-buffer buf
+        (when (eq mode major-mode)
+          (push buf buffer-mode-matches))))
+    buffer-mode-matches))
+
+;;--- Scratch Buffer -------------------------------------------------
+
+;; Create a new scratch buffer in given mode.
+;;;###autoload
+(defun nvp-buffer-scratch (mode)
+  (interactive (list (if current-prefix-arg
+                         (intern
+                          (ido-completing-read
+                           "Major mode: "
+                           (let (r)
+                             (mapatoms
+                              (lambda (x)
+                                (when (s-suffix? "-mode"
+                                                 (symbol-name x))
+                                  (push x r))))
+                             (mapcar 'symbol-name r))))
+                       'emacs-lisp-mode)))
+  (let ((n 0)
+        bufname)
+    (while (progn
+             (setq bufname (concat "*scratch*"
+                                   (if (= n 0) "" (int-to-string n))
+                                   "*"))
+             (setq n (1+ n))
+             (get-buffer bufname)))
+    (switch-to-buffer (get-buffer-create bufname))
+    (call-interactively mode)))
+
+;; Create scratch for current mode.
+;;;###autoload
+(defun nvp-buffer-scratch-current-mode ()
+  (interactive)
+  (nvp-buffer-scratch major-mode))
+
+;;--- Buffer Files ---------------------------------------------------
+
+;; Delete the current file, and kill the buffer.
+;;;###autoload
+(defun nvp-buffer-delete-file ()
+  (interactive)
+  (or (buffer-file-name) (error "No file is currently being edited"))
+  (when (y-or-n-p (format "Really delete '%s'? "
+                          (file-name-nondirectory buffer-file-name)))
+    (delete-file (buffer-file-name))
+    (kill-this-buffer)))
+
+;; Renames both the current buffer and file it's visiting to
+;; `NEW-NAME'.
+;;;###autoload
+(defun nvp-buffer-rename-file (new-name)
+  (interactive "sNew name:")
+  (let ((name (buffer-name))
+	(filename (buffer-file-name)))
+    (unless filename
+      (error "Buffer '%s' is not visiting a file!" name))
+    (if (get-buffer new-name)
+	(message "A buffer named '%s' already exists!" new-name)
+      (progn 
+	(when (file-exists-p filename)
+	  (rename-file filename new-name 1))
+	(rename-buffer new-name)
+	(set-visited-file-name new-name)))))
+
+;; -------------------------------------------------------------------
+
+(declare-function s-suffix? "s")
+
+(provide 'nvp-buffer)
+;;; nvp-buffer.el ends here
---
+++ nvp-jump.el
@@ -0,0 +1,172 @@
+;;; nvp-jump --- 
+
+;; This is free and unencumbered software released into the public domain.
+
+;; Author: Noah Peart <noah.v.peart@gmail.com>
+;; URL: https://github.com/nverno/nvp
+;; Package-Requires: 
+;; Created: 24 November 2016
+
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;; Code:
+(eval-when-compile
+  (require 'cl-lib)
+  (require 'nvp-macro)
+  (require 'nvp-local nil t)
+  (defvar recentf-list))
+(autoload 'elisp-utils-elisp-follow "elisp-utils")
+
+;;--- Modes ----------------------------------------------------------
+
+;;;###autoload
+(defun nvp-jump-to-mode-config (mode)
+  (interactive
+   (list 
+    (ido-completing-read
+     "Mode: "
+     (mapcar
+      #'(lambda (x)
+          (replace-regexp-in-string "\\(nvp-\\|\\.el\\)" ""
+                                    x))
+      (directory-files nvp/mode nil "^[^\\.].*\\.el$")))))
+  (let ((src (format "nvp-%s.el" mode)))
+    (find-file-other-window (expand-file-name src nvp/mode))))
+
+
+;; Jump to test with extension `STR'.  If it doesn't exist make a new
+;; file, and if there are multiple matches offer ido choice.
+;;;###autoload
+(defun nvp-jump-to-mode-test (str)
+  (interactive "sExtension:")
+  (let* ((test-files (directory-files nvp/test
+                                      t (concat "\\." str "$")))
+         (alt-files (or (and (or (not test-files))
+                             (directory-files-recursively
+                              nvp/test (concat "\\." str "$") nil))
+                        (concat
+                         nvp/test (format "test_%s.%s" str str))))
+         (files (or test-files alt-files))
+         (file (if (and (listp files) (> (length files) 1))
+                   (ido-completing-read "Options: " files)
+                 (or (and (eq 1 (length files))
+                          (car files))
+                     files))))
+    (find-file-other-window file)))
+
+;; Jump to where the hook for this mode is defined.
+;;;###autoload
+(defun nvp-jump-to-mode-hook ()
+  (interactive)
+  (let* ((str-mode-hook (format "%s-hook" major-mode))
+         (hook-fn-name
+          (format "nvp-%s-hook" (substring (symbol-name major-mode)
+                                           0 -5)))
+         (hook-fn (intern-soft hook-fn-name)))
+    (if hook-fn
+        (elisp-utils-elisp-follow hook-fn-name)
+      (find-file (expand-file-name "nvp-mode-hooks.el" nvp/lisp))
+      (goto-char (point-min))
+      (search-forward str-mode-hook nil t))))
+
+;;--- Libraries ------------------------------------------------------
+
+;;;###autoload
+(defun nvp-jump-to-library (library)
+  (interactive
+   (list (completing-read "Locate library: "
+                          (apply-partially
+                           'locate-file-completion-table
+                           load-path (get-load-suffixes)))))
+  (let* ((file (locate-file library load-path
+                            (append (get-load-suffixes)
+                                    load-file-rep-suffixes)))
+         (el (concat (file-name-sans-extension file) ".el"))
+         (elgz (concat el ".gz")))
+    ;; if no .el, on linux, jump to .el.gz instead
+    (find-file-other-window
+     (if (file-exists-p el) el elgz))))
+
+;;--- Org ------------------------------------------------------------
+
+;;;###autoload
+(defun nvp-jump-to-org (arg)
+  (interactive "P")
+  (let* ((org (or (and arg
+                       (ido-completing-read
+                        "Org file: "
+                        (directory-files nvp/org nil "^[^.]")))
+                  "gtd.org"))
+         (file (expand-file-name org nvp/org)))
+    (when (file-exists-p file)
+      (find-file-other-window file)
+      (goto-char (point-min))
+      (search-forward "* Notes" nil 'move))))
+
+;;--- Book -----------------------------------------------------------
+
+;; Jump to book in directory:
+;; 1. `nvp/books'/DIRNAME if non-nil.
+;; 2. Prompt for directory with prefix arg (appends `nvp/books')
+;; 3. Check for variable 'books-directory'
+;; 4. Default to 'nvp/books'.
+;;;###autoload
+(defun nvp-jump-to-book (dirname)
+  (interactive
+   (list (or
+          (bound-and-true-p dirname)
+          (and current-prefix-arg
+               (expand-file-name
+                (read-from-minibuffer
+                 "Directory Name (default book root): ")
+                nvp/books))
+          (and (bound-and-true-p books-directory)
+               (expand-file-name books-directory nvp/books))
+          nvp/books)))
+  (let* ((files (directory-files dirname t "^[^.]"))
+         (mode (substring-no-properties (symbol-name major-mode)
+                                        0 -5))
+         (case-fold-search t))
+    (when files
+      (if (and mode
+               (member mode (mapcar #'file-name-nondirectory files)))
+          (ido-find-file-in-dir (expand-file-name mode dirname))
+        (ido-find-file-in-dir dirname)))))
+
+;;--- Bookmarks ------------------------------------------------------
+
+;;;###autoload
+(defun nvp-bookmark-local (file)
+  (interactive
+   (list (or
+          (bound-and-true-p file)
+          (and current-prefix-arg
+               (read-file-name "Location of bookmark file: "))
+          (and (bound-and-true-p local-bookmarks)
+               (expand-file-name
+                local-bookmarks
+                (locate-dominating-file default-directory dir-locals-file)))
+          bookmark-default-file)))
+  (message "Current bookmarks file is: %s" file)
+  (when (not (string= bookmark-default-file file))
+    (bmk-to-bmk-handler (bmk-to-bmk-make-record file)))
+  (call-interactively 'bookmark-bmenu-list))
+
+(provide 'nvp-jump)
+;;; nvp-jump.el ends here

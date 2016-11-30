7ad47606f5cedeabeb7239321123530dc7a388cc
nverno <noah.v.peart@gmail.com>
2016-11-30 00:07:24 -0500

    squash

---
+++ nvp-apropos.el
@@ -0,0 +1,61 @@
+;;; nvp-apropos --- 
+
+;; This is free and unencumbered software released into the public domain.
+
+;; Author: Noah Peart <noah.v.peart@gmail.com>
+;; URL: https://github.com/nverno/nvp
+;; Package-Requires: 
+;; Created: 30 November 2016
+
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;; Code:
+(eval-when-compile
+  (require 'hydra))
+
+;;;###autoload
+(defun nvp-apropos-init ()
+  (interactive)
+  (nvp-apropos-hydra/body))
+
+(global-set-key (kbd "<f2> h a") #'nvp-help-apropos/body)
+
+(defhydra nvp-apropos (:color blue)
+  "Apropos"
+  ("a" apropos "apropos")
+  ("c" apropos-command "cmd")
+  ("d" apropos-documentation "doc")
+  ("e" apropos-value "val")
+  ("l" apropos-library "lib")
+  ("o" apropos-user-option "option")
+  ("u" apropos-user-option "option")
+  ("v" apropos-variable "var")
+  ("i" info-apropos "info")
+  ("t" xref-find-apropos "tags")
+  ("z" nvp-apropos-customize/body "customize"))
+
+(defhydra nvp-apropos-customize (:color blue)
+  "Apropos (customize)"
+  ("a" customize-apropos "apropos")
+  ("f" customize-apropos-faces "faces")
+  ("g" customize-apropos-groups "groups")
+  ("o" customize-apropos-options "options"))
+
+(provide 'nvp-apropos)
+;;; nvp-apropos.el ends here

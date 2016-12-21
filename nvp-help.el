ce151036fbf11c4ef907cc43e72d95a236f6bd3a
nverno <noah.v.peart@gmail.com>
2016-12-21 13:40:52 -0500

    squash

--- nvp-help.el
+++ nvp-help.el
@@ -66,7 +66,7 @@
       (message "No face at %d" pos))))
 
 ;;--- Docset ---------------------------------------------------------
-
+(autoload 'zeal-at-point-run-search "zeal-at-point")
 (defun nvp-help-zeal-run-search (search)
   (if zeal-at-point-exe
       (if (version< "0.2.0" (zeal-at-point-get-version))

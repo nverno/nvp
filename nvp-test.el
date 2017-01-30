ef3eacf512c3a453fc479815fc734aa85a319bbd
nverno <noah.v.peart@gmail.com>
2017-01-29 22:35:36 -0500

    squash

--- nvp-test.el
+++ nvp-test.el
@@ -35,6 +35,12 @@
 ;; -------------------------------------------------------------------
 ;;; Find test files 
 
+;; called when visiting new test buffer with no arguments
+(defvar nvp-test-init-function 'ignore)
+
+;; called when visiting test buffer with no arguments
+(defvar nvp-test-init-buffer-function 'ignore)
+
 ;; create test directory in project root or local root if doesn't exist
 (defun nvp-test-create-test-dir (&optional local dir)
   (let ((test-dir (expand-file-name
@@ -46,54 +52,58 @@
 ;; possible test directories (choose first found):
 ;; - test tests t
 (defun nvp-test-dir (&optional local create test-dirs)
-  (when-let ((root (nvp-project-locate-root local)))
-    (let* ((default-directory root)
-           (test-dirs (or test-dirs nvp-project--test-dir))
-           (dir (cl-find-if #'file-exists-p test-dirs)))
-      (if dir (expand-file-name dir)
-        (and create test-dirs
-             (nvp-test-create-test-dir local (car test-dirs)))))))
+  (if create
+      (nvp-test-create-test-dir
+       local (car (or test-dirs nvp-project--test-dir)))
+    (when-let ((root (nvp-project-locate-root local)))
+      (let* ((default-directory root)
+             (test-dirs (or test-dirs nvp-project--test-dir))
+             (dir (cl-find-if #'file-exists-p test-dirs)))
+        (and dir (expand-file-name dir))))))
 
 ;; list of elisp test files in project test folder
-(defun nvp-test--test-files (&optional local create test-dirs test-re)
-  (when-let ((test (nvp-test-dir local create test-dirs)))
-    (directory-files test t (or test-re nvp-project--test-re))))
+(defun nvp-test--test-files (dir &optional test-re)
+  (directory-files dir t (or test-re nvp-project--test-re)))
 
 ;; If test file found that matches buffer-file-name, return that,
 ;; otherwise prompt with completing-read 
-(defun nvp-test-find-test (file &optional local create prefixes suffixes
-                                test-dirs test-re)
-  (let ((files (nvp-test--test-files local create test-dirs test-re))
+(defun nvp-test-find-matching-test (file test-dir &optional prefixes suffixes)
+  (let ((default-directory test-dir)
+        (files (directory-files test-dir t "^[^.]"))
         (basename (file-name-nondirectory (file-name-sans-extension file))))
-    (when files
-      (or 
-       (cl-find-if
-        (lambda (f)
-          (cl-member
-           (file-name-sans-extension (file-name-nondirectory f))
-           (append
-            (mapcar (lambda (prefix) (concat prefix basename))
-                    (or prefixes '("t-" "t_" "test-" "test_")))
-            (mapcar (lambda (suffix) (concat basename suffix))
-                    (or suffixes '("-test" "_test"))))
-           :test 'string=))
-        files)
-       (let ((test-dir (file-name-directory (car files))))
-         (expand-file-name
-          (funcall-interactively 'ido-completing-read "Test file: "
-                                 (mapcar 'file-name-nondirectory files))
-          test-dir))))))
+    (or 
+     ;; try to find test matching current buffer's name with test prefix/suffixes
+     (cl-find-if
+      (lambda (f)
+        (cl-member
+         (file-name-sans-extension (file-name-nondirectory f))
+         (append
+          (mapcar (lambda (prefix) (concat prefix basename))
+                  (or prefixes '("t-" "t_" "test-" "test_")))
+          (mapcar (lambda (suffix) (concat basename suffix))
+                  (or suffixes '("-test" "_test"))))
+         :test 'string=))
+      files)
+     ;; read name of new test
+     (expand-file-name
+      (funcall-interactively 'ido-completing-read "Test file: "
+                             (mapcar 'file-name-nondirectory files))
+      default-directory))))
 
-(defmacro nvp-with-test (&optional local create no-test prefixes suffixes
-                                   &rest body)
+(defmacro nvp-with-test (&optional local create no-test prefixes suffixes &rest body)
   "Do BODY in project test file, prompting if more than one is found.
 Do NO-TEST if no tests are found, default to user-error."
   (declare (indent defun))
-  `(let ((test-file (nvp-test-find-test (buffer-file-name) ,local ,create
-                                        ,prefixes ,suffixes)))
+  `(let* ((test-file
+           (nvp-test-find-matching-test (buffer-file-name)
+                                        (nvp-test-dir ,local ,create)
+                                        ,prefixes ,suffixes))
+          (new-file (not (file-exists-p test-file))))
      (if (not test-file)
          ,(or no-test '(user-error "No test files found."))
        (with-current-buffer (find-file-noselect test-file)
+         (and new-file (funcall nvp-test-init-function))
+         (funcall nvp-test-init-buffer-function)
          ,@body))))
 
 ;; -------------------------------------------------------------------

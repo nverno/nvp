8c936b1d773d34a0cf28ed5400c93ef55e3c53bf
nverno <noah.v.peart@gmail.com>
2017-03-30 20:12:39 -0400

    squash

--- nvp-session.el
+++ nvp-session.el
@@ -33,19 +33,35 @@
 (require 'desktop)
 
 (defsubst nvp-session--read (prompt)
-  (ido-completing-read
-   prompt
-   (directory-files (expand-file-name "sessions" nvp/cache) t "^[.][^.]")))
+  (list
+   (expand-file-name "sessions" nvp/cache)
+   (ido-completing-read
+    prompt
+    (directory-files (expand-file-name "sessions" nvp/cache) nil "^.[^.]"))))
+
+(eval-when-compile
+  (defmacro with-desktop-vars (dirname filename &rest body)
+   (declare (indent defun))
+   `(let ((desktop-base-file-name (or ,filename ".emacs.desktop"))
+          (desktop-dirname ,dirname))
+      ,@body)))
 
 ;;;###autoload
-(defun nvp-session-save (&optional name)
-  (interactive (list (nvp-session--read "Save session: ")))
-  (desktop-save name))
+(defun nvp-session-save (&optional dirname name)
+  (interactive (nvp-session--read "Save session: "))
+  (with-desktop-vars dirname name
+    (desktop-save dirname)))
 
 ;;;###autoload
-(defun nvp-session-load (&optional name)
-  (interactive (list (nvp-session--read "Load session: ")))
-  (desktop-read name))
+(defun nvp-session-load (&optional dirname name)
+  (interactive (nvp-session--read "Load session: "))
+  (with-desktop-vars dirname name
+    (desktop-read dirname)))
+
+;;;###autoload
+(defun nvp-session-delete (&optional dirname name)
+  (interactive (nvp-session--read "Delete session: "))
+  (delete-file (expand-file-name name dirname)))
 
 (provide 'nvp-session)
 ;;; nvp-session.el ends here

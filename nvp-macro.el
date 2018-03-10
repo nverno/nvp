390d0b079847abd10a93e244d0dc303727eb6f30
nverno <noah.v.peart@gmail.com>
2018-03-10 00:42:41 -0500

    squash

--- nvp-macro.el
+++ nvp-macro.el
@@ -183,9 +183,11 @@ line at match (default) or do BODY at point if non-nil."
     `,@gnu))
 
 ;; -------------------------------------------------------------------
-;;; Config
+;;; Programs / Paths
 
-(defmacro nvp-program (name &optional no-compile)
+;; PATH can contain environment variables to expand
+;; if NO-COMPILE is defined the name is determined at runtime
+(defmacro nvp-program (name &optional no-compile path)
   (let ((name (cond
                ((symbolp name) (symbol-name name))
                ((consp name)
@@ -194,14 +196,17 @@ line at match (default) or do BODY at point if non-nil."
                    (symbol-name sym))
                   (_ name)))
                ((stringp name) name)
-               (t (user-error "%S unmatched")))))
+               (t (user-error "%S unmatched"))))
+        (path (and path (substitute-env-in-file-name path))))
     `(,(if no-compile 'progn 'eval-when-compile)
       (if (eq system-type 'windows-nt)
           (bound-and-true-p
            ,(intern (concat "nvp-" name "-program")))
-        (let ((dotlocal (expand-file-name ,name "~/.local/bin"))
+        (let ((path (and ,path (expand-file-name ,name ,path)))
+              (dotlocal (expand-file-name ,name "~/.local/bin"))
               (local (expand-file-name ,name "/usr/local/bin")))
           (or
+           (and path (file-exists-p path) path)
            (and (file-exists-p dotlocal) dotlocal)
            (and (file-exists-p local) local)
            (executable-find ,name)))))))

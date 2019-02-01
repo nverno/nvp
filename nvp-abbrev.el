6300aa99844cc4db6c94ac892e1aaeb04effb9fc
nverno <noah.v.peart@gmail.com>
2019-02-01 00:10:44 -0500

    squash

--- nvp-abbrev.el
+++ nvp-abbrev.el
@@ -1,9 +1,10 @@
-;;; nvp-abbrev ---  -*- lexical-binding: t; -*-
+;;; nvp-abbrev.el ---  -*- lexical-binding: t; -*-
 
 ;; This is free and unencumbered software released into the public domain.
 
 ;; Author: Noah Peart <noah.v.peart@gmail.com>
 ;; URL: https://github.com/nverno/nvp
+;; Last modified: <2019-01-31 21:39:00>
 ;; Package-Requires: 
 ;; Created: 24 November 2016
 

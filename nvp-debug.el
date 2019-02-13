f23bb9138ab3c7578a042089358a419d99e756ef
nverno <noah.v.peart@gmail.com>
2019-02-12 22:24:07 -0500

    squash

--- nvp-debug.el
+++ nvp-debug.el
@@ -4,7 +4,7 @@
 
 ;; Author: Noah Peart <noah.v.peart@gmail.com>
 ;; URL: https://github.com/nverno/nvp
-;; Last modified: <2019-02-12 20:02:12>
+;; Last modified: <2019-02-12 22:18:32>
 ;; Package-Requires: 
 ;; Created: 25 November 2016
 
@@ -29,6 +29,7 @@
 ;;; Code:
 (eval-when-compile
   (require 'nvp-macro)
+  (nvp-local-vars)
   (defvar tramp-debug-on-error)
   (defvar tramp-verbose)
   (require 'hydra))
@@ -44,11 +45,11 @@ Toggle debugging functions:
 toggle on _e_rror              _a_: all defuns         _t_ramp
 toggle on _q_uit               _l_: all local defs     _i_: ielm
 _n_ew emacs (in debug)         _f_: all forms          _SPC_: cancel
-_b_are emas (-Q
-toggle on entr_y_              _c_urrent defun
-_d_ebugger
+_b_are emacs (-l bare-site)    _c_urrent defun         _d_ebugger
+toggle on entr_y_
 "
   ("a" edebug-all-defs)
+  ("b" nvp-debug-launch-bare-emacs)
   ("c" edebug-defun :color blue)
   ("d" debug :color blue)
   ("e" toggle-debug-on-error)
@@ -60,14 +61,6 @@ _d_ebugger
   ("SPC" nil)
   ("t" nvp-debug-tramp-toggle-debug)
   ("y" debug-on-entry :color blue))
-
-(defun nvp-debug-launch-new-debug ()
-  "Start new emacs in debug mode."
-  (interactive)
-  (call-process-shell-command 
-   (concat (shell-quote-argument (nvp-with-gnu/w32 "emacs" "runemacs.exe")) " "
-	   (shell-quote-argument "--debug-init"))
-   nil 0 nil))
 
 (defvar nvp-ede-local-active nil)
 
@@ -83,19 +76,31 @@ _d_ebugger
   "Toggle `tramp-debug-on-error' on/off."
   (interactive)
   (ignore-errors
-   (when (and (file-remote-p (buffer-file-name)))
-     (let ((action (and (not (null tramp-debug-on-error))
-                        tramp-debug-on-error)))
-       (setq tramp-debug-on-error (not action))
-       (setq tramp-verbose (if action 0 10))
-       (message "set `tramp-debug-on-error' %s" (if action "off" "on"))))))
+    (when (and (file-remote-p (buffer-file-name)))
+      (let ((action (and (not (null tramp-debug-on-error))
+                         tramp-debug-on-error)))
+        (setq tramp-debug-on-error (not action))
+        (setq tramp-verbose (if action 0 10))
+        (message "set `tramp-debug-on-error' %s" (if action "off" "on"))))))
+
+;;; Launching new emacs instances
+(eval-when-compile
+  (defmacro nvp-debug--launch-emacs (&rest args)
+    (declare (debug defun))
+    (let ((args (cons (nvp-with-gnu/w32 "emacs" "runemacs.exe") args)))
+      `(call-process-shell-command
+        (mapconcat #'shell-quote-argument (list ,@args) " ") nil 0 nil))))
 
 (defun nvp-debug-launch-bare-emacs ()
-  "Start new emacs instance loading bare init file."
+  "Start new emacs instance loading init file that only adds load paths."
   (interactive)
-  (apply #'shell-quote-argument
-   (nvp-with-gnu/w32 "emacs" "runemacs.exe")) 
-  )
+  (nvp-debug--launch-emacs
+   "-Q" "-l" (expand-file-name "bare-site.el" nvp/etc)))
+
+(defun nvp-debug-launch-new-debug ()
+  "Start new emacs in debug mode."
+  (interactive)
+  (nvp-debug--launch-emacs "--debug-init"))
 
 (provide 'nvp-debug)
 ;;; nvp-debug.el ends here

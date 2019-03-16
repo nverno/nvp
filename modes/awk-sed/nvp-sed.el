07a3bf38d576179c1ced7117038f7a23539b05a6
nverno <noah.v.peart@gmail.com>
2019-03-16 00:45:22 -0400

    squash

--- modes/awk-sed/nvp-sed.el
+++ modes/awk-sed/nvp-sed.el
@@ -1,8 +1,6 @@
 ;;; nvp-sed.el --- sed -*- lexical-binding: t; -*-
 
-;; This is free and unencumbered software released into the public domain.
-
-;; Last modified: <2019-03-15 18:22:04>
+;; Last modified: <2019-03-16 00:44:09>
 ;; Author: Noah Peart <noah.v.peart@gmail.com>
 ;; URL: https://github.com/nverno/nvp
 ;; Created: 15 March 2019
@@ -19,6 +17,9 @@
   (interactive)
   (let ((url "https://www.gnu.org/software/sed/manual/sed.html"))
     (browse-url url)))
+
+(defun nvp-sed-setup-locals ()
+  (setq imenu-generic-expression '((nil "^:\\([^ \n\f\t]+\\)" 1))))
 
 (provide 'nvp-sed)
 ;; Local Variables:

b2bd0386d735dc80f0201f0735fbceb85d7fa2ee
nverno <noah.v.peart@gmail.com>
2019-03-21 14:23:23 -0400

    squash

--- modes/python/nvp-python-debug.el
+++ modes/python/nvp-python-debug.el
@@ -1,29 +1,9 @@
-;;; nvp-python-debug.el ---  -*- lexical-binding: t; -*-
-
-;; This is free and unencumbered software released into the public domain.
+;;; nvp-python-debug.el --- python debugging -*- lexical-binding: t; -*-
 
 ;; Author: Noah Peart <noah.v.peart@gmail.com>
-;; URL: https://github.com/nverno/python-tools
-;; Last modified: <2019-02-20 13:25:07>
-;; Package-Requires: 
+;; URL: https://github.com/nverno/nvp
+;; Last modified: <2019-03-21 14:17:54>
 ;; Created: 16 September 2018
-
-;; This file is not part of GNU Emacs.
-;;
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 3, or
-;; (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
-;; Floor, Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 ;;; Code:

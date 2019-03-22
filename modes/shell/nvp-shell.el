0201286b5bd3bd4d89469d2b1f5465d7916082ac
nverno <noah.v.peart@gmail.com>
2019-03-22 05:05:19 -0400

    squash

--- modes/shell/nvp-shell.el
+++ modes/shell/nvp-shell.el
@@ -1,6 +1,6 @@
 ;;; nvp-shell.el --- shell helpers -*- lexical-binding: t; -*-
 
-;; Last modified: <2019-03-15 16:11:04>
+;; Last modified: <2019-03-22 03:22:00>
 ;; Author: Noah Peart <noah.v.peart@gmail.com>
 ;; URL: https://github.com/nverno/nvp
 ;; Created:  4 November 2016
@@ -19,30 +19,31 @@
 ;; -------------------------------------------------------------------
 ;;; Utils
 
-(defmacro nvp-shell-goto-command-start (start &optional limit delims)
-  "Move point to beginning of current command.
+(eval-when-compile
+ (defmacro nvp-shell-goto-command-start (start &optional limit delims)
+   "Move point to beginning of current command.
 START is the initial point, LIMIT is an optional search bound.
 DELIMS are chars that will delimit commands and won't be skipped outside of
 strings."
-  (or delims (setq delims "^)(|&\`;\["))
-  (macroexp-let2 nil limit limit
-   `(let (ppss done)
-      (while (and (not done) ,(if limit `(> (point) ,limit) t))
-        (skip-chars-backward ,delims ,(or limit '(line-beginning-position)))
-        (if (eq (char-before) ?\))
-            (forward-sexp -1)           ; jump back over a possible subshell
-          (setq ppss (parse-partial-sexp ,(or limit '(point-min)) (point)))
-          (cond
-           ;; presumably reached the beginning of a command
-           ((or (not (nth 3 ppss))
-                (eq (char-before) ?\`)
-                (and (eq (char-before) ?\()
-                     (eq (char-before (1- (point))) ?$)))
-            (setq done t))
-           ;; move backward out of enclosing string that shouldn't be a quoted
-           ;; command
-           (t (up-list -1 t t)))))
-      (skip-syntax-forward " " ,start))))
+   (or delims (setq delims "^)(|&\`;\["))
+   (macroexp-let2 nil limit limit
+     `(let (ppss done)
+        (while (and (not done) ,(if limit `(> (point) ,limit) t))
+          (skip-chars-backward ,delims ,(or limit '(line-beginning-position)))
+          (if (eq (char-before) ?\))
+              (forward-sexp -1)         ; jump back over a possible subshell
+            (setq ppss (parse-partial-sexp ,(or limit '(point-min)) (point)))
+            (cond
+             ;; presumably reached the beginning of a command
+             ((or (not (nth 3 ppss))
+                  (eq (char-before) ?\`)
+                  (and (eq (char-before) ?\()
+                       (eq (char-before (1- (point))) ?$)))
+              (setq done t))
+             ;; move backward out of enclosing string that shouldn't be a quoted
+             ;; command
+             (t (up-list -1 t t)))))
+        (skip-syntax-forward " " ,start)))))
 
 ;; -------------------------------------------------------------------
 ;;; Things-at-point

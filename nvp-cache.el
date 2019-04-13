6618c2702f895071d7f3d155cb907a00c66d1e61
nverno <noah.v.peart@gmail.com>
2019-04-13 17:07:09 -0400

    squash

--- nvp-cache.el
+++ nvp-cache.el
@@ -1,10 +1,11 @@
-;;; nvp-cache.el --- Caching helpers -*- lexical-binding: t; -*-
+;;; nvp-cache.el --- Generic caches -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 
-;; Generic cache
+;; Current cache backends:
+;; - ring
+;; - hash
 
-;; FIXME: mostly unused
 ;; Hash table with expiring entries:
 ;; https://github.com/skeeto/skewer-mode/blob/master/cache-table.el
 
@@ -13,9 +14,7 @@
   (require 'cl-lib)
   (require 'nvp-macro))
 
-;; TODO: get rid of these
-(defvar nvp-ring-default-size 50)
-(defvar nvp-ring-separator "\n")
+;; (defmacro nvp-with-struct-slots (struct ))
 
 (defsubst nvp-cache--names (struct-type)
   (declare (pure t) (side-effect-free t))
@@ -68,7 +67,8 @@
         cache)))))
 
 (cl-defgeneric nvp-cache-length (cache)
-  "Number of elements in CACHE.")
+  "Number of elements in CACHE."
+  (declare (pure t) (side-effect-free t)))
 
 (cl-defmethod nvp-cache-length ((cache nvp-cache-ring))
   (ring-length (nvp-cache-ring-data cache)))
@@ -78,6 +78,7 @@
 
 (cl-defgeneric nvp-cache-empty-p (cache)
   "Non-nil if CACHE contains no elements."
+  (declare (pure t) (side-effect-free t))
   (zerop (nvp-cache-length cache)))
 
 (cl-defgeneric nvp-cache-elt (elt cache &optional _default)
@@ -92,11 +93,27 @@
   "Retrieve KEY's value from hash CACHE."
   (gethash elt (nvp-cache-hash-data cache) default))
 
-(cl-defmethod (setf (cache nvp-cache-elt)) (val cache key)
+(cl-defmethod (setf nvp-cache-elt) (val key (cache nvp-cache-hash))
   (puthash key val (nvp-cache-hash-data cache)))
-;; (gv-define-setter nvp-cache-get (key val (cache nvp-cache-hash))
-;;   "Add KEY-VAL pair to hash CACHE using `setf'."
-;;   `(puthash ,key ,val (nvp-cache-hash-data ,cache)))
+
+(cl-defmethod (setf nvp-cache-elt) (val elt (cache nvp-cache-ring))
+  (let* ((ring (nvp-cache-ring-data cache))
+         (len (ring-length ring)))
+    (if (< elt len)
+        (aset (cddr (nvp-cache-ring-data cache)) elt val)
+      (user-error "ring cache doesn't have %d elements." elt))))
+
+(cl-defgeneric nvp-cache-insert (elt cache &optional val)
+  "Insert new element into cache.")
+
+(cl-defmethod nvp-cache-insert (elt (cache nvp-cache-ring) &optional _val)
+  "Insert element as most recent in ring cache.
+Grows ring when necessary."
+  (nvp-ring--insert (nvp-cache-ring-data cache) elt
+                    (nvp-cache-ring-default-size cache)))
+
+(cl-defmethod nvp-cache-insert (key (cache nvp-cache-hash) &optional val)
+  (puthash key val (nvp-cache-hash-data cache)))
 
 (cl-defgeneric nvp-cache-map (f cache)
   "Apply function F to elements of CACHE.")
@@ -104,17 +121,17 @@
 (cl-defmethod nvp-cache-map (f (cache nvp-cache-hash))
   (maphash (lambda (k v) (funcall f k v)) (nvp-cache-hash-data cache)))
 
-(defun nvp-cache-count (cache)
-  "Return number of CACHE entries."
-  (hash-table-count (nvp-cache-table cache)))
+(cl-defmethod nvp-cache-map (f (cache nvp-cache-ring))
+  (nvp-ring--map f (nvp-cache-ring-data cache)))
+
+(cl-defgeneric nvp-cache-read (cache &optional filename)
+  "Read data from cache file.")
+
+;; (cl-defmethod nvp-cache-read ((cache nvp-cache-ring) &optional filename)
+;;   (nvp-ring-r))
 
 ;; -------------------------------------------------------------------
 ;;; Ring
-
-(defsubst nvp-ring-ref (ring)
-  (ring-ref (nvp-ring-ring ring) (nvp-ring-idx ring)))
-(defsubst nvp-ring--extend (ring size)
-  (ring-extend ring (min size (- nvp-ring-default-size size))))
 
 ;; wrappers for ring access - not need to check equality for all items in ring
 ;; if storing the current index
@@ -124,70 +141,82 @@
 (defsubst nvp-ring-previous-index (ring idx)
   (ring-minus1 idx (ring-length ring)))
 
-(defun nvp-ring-insert (ring item)
+(defsubst nvp-ring--extend (ring size default-size)
+  (ring-extend ring (min size (- default-size size))))
+
+(defun nvp-ring--insert (ring item default-size)
   "Insert new item, growing ring if necessary."
   (let ((size (ring-size ring)))
     (and (= size (ring-length ring))
-         (nvp-ring--extend ring size)))
+         (nvp-ring--extend ring size default-size)))
   (ring-insert ring item))
 
-(defun nvp-ring-read (filename &optional size silent)
+(defun nvp-ring--map (f ring)
+  (let ((start (car ring))
+        (size (ring-size ring))
+        (vect (cddr ring)))
+    (dotimes (var (cadr ring))
+      (funcall f (aref vect (mod (+ start var size)))))))
+
+(defun nvp-ring-read (cache)
   "Read and return ring from FILENAME."
-  (cond
-   ((not (file-writable-p filename))
-    (or silent (message "Cant read %S" filename)))
-   (t
-    (let* ((count 0)
-           (size (min nvp-ring-default-size (or size nvp-ring-default-size)))
-           (ring (make-ring size)))
-      (with-temp-buffer
-        (insert-file-contents filename)
-        (goto-char (point-max))
-        (let (start end history)
-          (while (and (< count size)
-                      (re-search-backward nvp-ring-separator nil t)
-                      (setq end (match-end 0)))
-            (setq start
-                  (if (re-search-backward nvp-ring-separator nil t)
-                      (match-end 0)
-                    (point-min)))
-            (setq history (nvp-s 'bs start end))
-            (goto-char start)
-            ;; skip dupes
-            (when (not (string= (ring-ref ring 0) history))
-              (when (= count size)
-                (nvp-ring--extend ring size)
-                (setq size (ring-size ring)))
-              (ring-insert-at-beginning ring history)
-              (cl-incf count))))
-        ring)))))
+  (let 
+   (cond
+    ((not (file-writable-p filename))
+     (or silent (message "Cant read %S" filename)))
+    (t
+     (let* ((count 0)
+            (ring (make-ring size))
+            (sep (or separator "\n")))
+       (with-temp-buffer
+         (insert-file-contents filename)
+         (goto-char (point-max))
+         (let (start end history)
+           (while (and (< count size)
+                       (re-search-backward sep nil t)
+                       (setq end (match-end 0)))
+             (setq start
+                   (if (re-search-backward sep nil t)
+                       (match-end 0)
+                     (point-min)))
+             (setq history (nvp-s 'bs start end))
+             (goto-char start)
+             ;; skip dupes
+             (when (not (string= (ring-ref ring 0) history))
+               (when (= count size)
+                 (nvp-ring--extend ring size size)
+                 (setq size (ring-size ring)))
+               (ring-insert-at-beginning ring history)
+               (cl-incf count))))
+         ring))))))
 
 ;; Save ring to files
 ;; If PREDICATE is non-nil, it is called with a single argument, a ring element
 ;; if it returns non-nil, the element is saved, otherwise it is skipped.
-(defun nvp-ring-write (ring filename &optional silent predicate)
+(defun nvp-ring-write (ring filename &optional silent predicate separator)
   (cond
    ((ring-empty-p ring) nil)
    ((not (file-writable-p filename))
     (or silent (message "cant write to %S" filename)))
    (t
-    (let ((idx (ring-length ring)))
+    (let ((idx (ring-length ring)) elem)
       (with-temp-buffer
         (while (> idx 0)
           (cl-decf idx)
-          (when (or (null predicate) (funcall predicate (ring-ref ring idx)))
-            (insert (ring-ref ring idx) nvp-ring-separator)))
+          (setq elem (ring-ref ring idx))
+          (when (or (null predicate) (funcall predicate elem))
+            (insert elem (or separator "\n"))))
         (write-region (buffer-string) nil filename nil 'no-message))))))
 
 ;; -------------------------------------------------------------------
 ;;; Hash
 ;; TODO: generalize
 
-(defun nvp-cache-to-alist (cache)
-  "Return cache as alist."
-  (cl-loop with cache = (nvp-cache-table cache)
-     for k being the hash-keys of cache using (hash-value v)
-     collect (cons k v)))
+;; (defun nvp-cache-to-alist (cache)
+;;   "Return cache as alist."
+;;   (cl-loop with cache = (nvp-cache-table cache)
+;;      for k being the hash-keys of cache using (hash-value v)
+;;      collect (cons k v)))
 
 ;; (defun nvp-cache-from-alist (alist &rest cache-args)
 ;;   "Build cache using CACHE-ARGS from values in ALIST."
@@ -196,24 +225,16 @@
 ;;       (setf (nvp-cache-get k cache) v))
 ;;     cache))
 
-(defun nvp-cache-map (f cache)
-  "Apply function F to elements of CACHE ala `maphash'."
-  (maphash (lambda (k v) (funcall f k v)) (nvp-cache-table cache)))
-
-(defun nvp-cache-count (cache)
-  "Return number of CACHE entries."
-  (hash-table-count (nvp-cache-table cache)))
-
-(defun nvp-cache-save (cache file)
-  "Save cache to file."
+(defun nvp-cache--save-lisp (cache file)
+  "Save cache as lisp."
   (with-temp-buffer
     (let (print-level print-length)
       (insert ";; -*- no-byte-compile: t -*-\n")
       (prin1 cache (current-buffer))
       (write-file file))))
 
-(defun nvp-cache-load (file)
-  "Load saved cache."
+(defun nvp-cache--read-lisp (file)
+  "Load dumped lisp cache."
   (with-temp-buffer
     (insert-file-contents file)
     (car (read-from-string (buffer-string)))))

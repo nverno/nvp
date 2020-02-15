d7aa6e3cad87b8977714b2b7f03a7a404879632e
nverno <noah.v.peart@gmail.com>
2020-02-14 19:22:23 -0500

    Prompt for custom label before adding pdf annotations

--- modes/pdf/nvp-pdf.el
+++ modes/pdf/nvp-pdf.el
@@ -1,21 +1,33 @@
 ;;; nvp-pdf.el ---  -*- lexical-binding: t; -*-
 ;;; Commentary:
 ;;; Code:
-(eval-when-compile
-  (require 'nvp-macro)
-  (defvar pdf-info-epdfinfo-program))
+(eval-when-compile (require 'nvp-macro))
 (require 'pdf-tools)
+(nvp-decls :v (pdf-info-epdfinfo-program
+               pdf-tools-auto-mode-alist-entry
+               pdf-annot-default-annotation-properties)
+           :f (pdf-annot-add-text-annotation))
 
-;; ------------------------------------------------------------
-;;; Setup
-
-(defvar pdf-tools-auto-mode-alist-entry)
+;; build if necessary
 (defun nvp-pdf-setup ()
   (unless (file-exists-p pdf-info-epdfinfo-program)
     (pdf-tools-install t t t)
     ;; (call-interactively 'nvp-pdf-install-epdfinfo))
     ;; add enty that `pdf-tools-install' would add
     (add-to-list 'auto-mode-alist pdf-tools-auto-mode-alist-entry)))
+
+;; -------------------------------------------------------------------
+;;; Annotations
+
+;; Read an annotation label when attaching
+(defun nvp-pdf-add-text-annotation (label)
+  (interactive 
+   (list
+    (read-string (format "Label (%s): " user-full-name) nil nil user-full-name)))
+  (let ((pdf-annot-default-annotation-properties
+         (cons `(t (label . ,label))
+               (cdr pdf-annot-default-annotation-properties))))
+    (call-interactively #'pdf-annot-add-text-annotation)))
 
 (provide 'nvp-pdf)
 ;;; nvp-pdf.el ends here

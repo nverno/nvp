c336a928b0abac72ea36c1cc6b8206670d30f216
nverno <noah.v.peart@gmail.com>
2021-01-09 19:13:51 -0800

    squash

---
+++ modes/web/nvp-ejs.el
@@ -0,0 +1,20 @@
+;;; nvp-ejs.el --- embedded js lodash/underscore -*- lexical-binding: t; -*-
+;;
+;;; Commentary:
+;;; Code:
+(eval-when-compile (require 'nvp-macro))
+(require 'nvp)
+(nvp-decls)
+
+;;;###autoload
+(define-minor-mode ejs-mode "EJS minor mode"
+  nil
+  :lighter " EJS"
+  (yas-activate-extra-mode 'ejs-mode))
+
+(provide 'nvp-ejs)
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
+;;; nvp-ejs.el ends here

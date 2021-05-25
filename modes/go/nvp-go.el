416cd83fd1447083eb44541629f6cadeb7218888
nverno <noah.v.peart@gmail.com>
2021-05-24 20:59:10 -0400

    pulse after godef jump

--- modes/go/nvp-go.el
+++ modes/go/nvp-go.el
@@ -6,16 +6,12 @@
 ;;; Code:
 (eval-when-compile (require 'nvp-macro))
 (require 'go-mode)
+(require 'xref)
 (nvp-decls :v (go-use-gocheck-for-testing))
 (nvp-auto "asdf" asdf-current-version)
 
-(defvar nvp-go-type-re
-  (nvp-re-opt '("int" "int8" "int16" "int32" "int64"
-                "uint" "uint8" "uint16" "uint32" "uint64"
-                "uintptr"
-                "float32" "float64"
-                "complex64" "complex128"
-                "bool" "byte" "rune" "string" "error")))
+(define-advice godef-jump (:after (&rest _args) "pulse")
+  (run-hooks 'xref-after-jump-hook))
 
 ;; collect imports
 (defun nvp-go-imports ()

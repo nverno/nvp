d19e3ac4e83c46a2e98b865f86cdf2468cfd4739
nverno <noah.v.peart@gmail.com>
2022-01-05 15:46:39 -0800

    add lisp-data-mode complete wrapper

---
+++ modes/emacs-lisp/nvp-elisp-data.el
@@ -0,0 +1,25 @@
+;;; nvp-elisp-data.el --- lisp-data  -*- lexical-binding: t; -*-
+;;
+;;; Commentary:
+;;; Code:
+(eval-when-compile (require 'nvp-macro))
+(require 'nvp)
+(require 'company-elisp)
+(nvp-decls)
+
+;; wrapper for company-complete in lisp-data-mode
+;; Note: lisp-data-mode is parent of emacs-lisp-mode so
+;; (put 'derived-parent-mode ...) results in infinite loop
+;; XXX: how to add-advice on local copy of function?
+(defun nvp-company-lisp-data (&rest args)
+  (interactive (list 'interactive))
+  (let* ((orig-fn (symbol-function 'company-elisp))
+         (major-mode 'emacs-lisp-mode))
+    (apply orig-fn args)))
+
+(provide 'nvp-elisp-data)
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
+;;; nvp-elisp-data.el ends here

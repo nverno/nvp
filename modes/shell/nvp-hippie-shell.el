1e88bc0ed40ab10db036f1b22e16844fbe1a4af1
nverno <noah.v.peart@gmail.com>
2022-01-14 16:13:15 -0800

    fix hippe expansion of shell aliases

--- modes/shell/nvp-hippie-shell.el
+++ modes/shell/nvp-hippie-shell.el
@@ -6,7 +6,7 @@
 (require 'nvp-shell)
 
 (defun nvp-shell-read-aliases (shell-cmd regex key val)
-  "SHELL-CMD is a string passed to `call-process-shell-command' to print \
+  "SHELL-CMD is a string passed to `call-process-shell-command' to print
 aliases. REGEX is used to match KEY VAL pairs that are added to a hash table."
   (let ((ht (make-hash-table :size 129 :test #'equal)))
     (with-temp-buffer
@@ -18,20 +18,28 @@ aliases. REGEX is used to match KEY VAL pairs that are added to a hash table."
                    (match-string-no-properties val) ht))
         ht))))
 
+(nvp-lazy-defvar nvp-shell--alias-completion-table
+  (lambda ()
+    (list
+     ;; for bash assume all aliases are of the form
+     ;; alias ..='cd ..'
+     ;; eg. the start and end with "'" that way don't have to
+     ;; worry about escaped single quotes when parsing
+     ;; aliases
+     (nvp-shell-read-aliases
+      "bash -ci alias" "^alias\\s-*\\([^=]+\\)='\\(.*\\)'$" 1 2)
+     (cons
+      "git"
+      (nvp-shell-read-aliases "git alias" "^\\([^=]+\\)=\\(.+\\)$" 1 2)))))
+
+(eval-when-compile
+  ;; get expansion of ALIAS with optional prefix CMD from alias TABLE
+  (defsubst nvp-shell--get-alias (alias table &optional cmd)
+   (gethash alias (if cmd (cdr (assoc cmd table)) (car table)))))
+
 (defvar nvp-shell-alias-completion-table
   (lambda (args)
-    (let ((ht
-           (list
-            ;; for bash assume all aliases are of the form
-            ;; alias ..='cd ..'
-            ;; eg. the start and end with "'" that way don't have to
-            ;; worry about escaped single quotes when parsing
-            ;; aliases
-            (nvp-shell-read-aliases
-             "bash -ci alias" "^alias\\s-*\\([^=]+\\)='\\(.*\\)'$" 1 2)
-            (cons
-             "git"
-             (nvp-shell-read-aliases "git alias" "^\\([^=]+\\)=\\(.+\\)$" 1 2)))))
+    (let ((ht (nvp-lazy-val nvp-shell--alias-completion-table)))
       (setq nvp-shell-alias-completion-table
             (nvp-he-completion-table
              (lambda (cmd)
@@ -41,9 +49,7 @@ aliases. REGEX is used to match KEY VAL pairs that are added to a hash table."
                    (or (caddr cmd) "")
                    (or (cdr (assoc (cadr cmd) ht)) (car ht))))
                  ('value
-                  (gethash
-                   (caddr cmd)
-                   (or (cdr (assoc (cadr cmd) ht)) (car ht))))))
+                  (nvp-shell--get-alias (caddr cmd) ht (cadr cmd)))))
              #'equal))
       (funcall nvp-shell-alias-completion-table args))))
 
@@ -82,27 +88,27 @@ aliases. REGEX is used to match KEY VAL pairs that are added to a hash table."
 (setq-default nvp-he-shell-alias-beg
               #'(lambda () (car (bounds-of-thing-at-point 'symbol))))
 
-(defun nvp-shell-alias-completion-table ()
-  nil)
-
 ;;;###autoload
 (defun nvp-he-try-expand-shell-alias (old)
   "Expand shell alias, like bash shell-expand-alias."
   (cl-block nil
     (unless old
-      (let ((beg (funcall nvp-he-shell-alias-beg)))
+      (let ((cmd (thing-at-point 'shell-cmd))
+            (beg (funcall nvp-he-shell-alias-beg)))
         (and (not beg) (cl-return))
         (he-init-string beg (point))
+        (when (string= cmd he-search-string) (setq cmd nil))
         (unless (he-string-member he-search-string he-tried-table)
           (setq he-tried-table (cons he-search-string he-tried-table)))
         (setq he-expand-list             ;completions from hash table
               (and (not (equal "" he-search-string))
-                   (delq nil
-                         (mapcar
-                          (lambda (abbr)
-                            (gethash abbr (nvp-shell-alias-completion-table) nil))
-                          (all-completions
-                           he-search-string (nvp-shell-alias-completion-table))))))))
+                   (let ((ht (nvp-lazy-val nvp-shell--alias-completion-table)))
+                     (delq nil
+                           (mapcar
+                            (lambda (abbr)
+                              (nvp-shell--get-alias abbr ht cmd))
+                            (funcall nvp-shell-alias-completion-table
+                                     (list 'complete cmd he-search-string)))))))))
     (while (and he-expand-list         ;remove seen strings from table
                 (he-string-member (car he-expand-list) he-tried-table t))
       (setq he-expand-list (cdr he-expand-list)))

b528a7dc97a84376ccb3bf05be4e72b9f433cff1
nverno <noah.v.peart@gmail.com>
2022-05-10 18:02:31 -0700

    feat: add hap-lsp

--- pkgs/hap/nvp-hap-lsp.el
+++ pkgs/hap/nvp-hap-lsp.el
@@ -5,21 +5,27 @@
 (eval-when-compile (require 'nvp-macro))
 (require 'nvp)
 (require 'nvp-hap)
-(nvp:decls :f (lsp-describe-thing-at-point))
+(nvp:decls :f (lsp--text-document-position-params
+               lsp-describe-thing-at-point
+               lsp--make-request lsp--send-request lsp:hover-contents
+               lsp--render-on-hover-content))
 
 ;;;###autoload
-(defun nvp-hap-lsp (command &optional art &rest _args)
+(defun nvp-hap-lsp (command &optional arg &rest _args)
   (cl-case command
-    (thingatpt ())
+    (thingatpt (let ((contents (-some->> (lsp--text-document-position-params)
+                                 (lsp--make-request "textDocument/hover")
+                                 (lsp--send-request)
+                                 (lsp:hover-contents))))
+                 (and contents (not (equal "" contents)) contents)))
+    (doc-string (string-trim-right (lsp--render-on-hover-content arg t)))
     (doc-buffer
-     (save-window-excursion
-       (let ((display-buffer-overriding-action
-              '(nil . ((inhibit-switch-frame . t)))))
-         (when (cond
-                ((fboundp arg) (describe-function arg))
-                ((boundp arg) (describe-variable arg))
-                (t nil))
-           (list (help-buffer) (point-min) nil)))))))
+     (let ((display-buffer-overriding-action
+            '(nil . ((inhibit-switch-frame . t))))
+           (lsp-help-buf-name "*lsp-help*"))
+       (with-current-buffer (get-buffer-create lsp-help-buf-name)
+         (insert (string-trim-right (lsp--render-on-hover-content arg t)))
+         (list (current-buffer) (point-at-bol) nil))))))
 
 (provide 'nvp-hap-lsp)
 ;; Local Variables:

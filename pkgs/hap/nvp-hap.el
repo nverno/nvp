e06da45e336850dbab7900512415ea25df902180
nverno <noah.v.peart@gmail.com>
2023-01-25 16:54:26 -0800

    fix: hap pos-top

--- pkgs/hap/nvp-hap.el
+++ pkgs/hap/nvp-hap.el
@@ -152,8 +152,82 @@
                    (window-point win)
                  (or (cadr buff) (point-min)))))))))
 
-;;; Popup
+;; -------------------------------------------------------------------
+;;; Popup 
 ;; popup looks for (1) doc-string then (2) doc-buffer
+
+;; From pos-tip.el
+;; (1/25/23) `propertize' messes up popup for some reason on emacs v30 build
+(defun nvp-pos-tip-show
+  (string &optional tip-color pos window timeout width frame-coordinates dx dy)
+  "Show STRING in a tooltip, which is a small X window, at POS in WINDOW
+using frame's default font with TIP-COLOR.
+
+Return pixel position of tooltip relative to top left corner of frame as
+a cons cell like (X . Y).
+
+TIP-COLOR is a face or a cons cell like (FOREGROUND-COLOR . BACKGROUND-COLOR)
+used to specify *only* foreground-color and background-color of tooltip. If
+omitted, use `pos-tip-foreground-color' and `pos-tip-background-color' or the
+foreground and background color of the `tooltip' face instead.
+
+Omitting POS and WINDOW means use current position and selected window,
+respectively.
+
+Automatically hide the tooltip after TIMEOUT seconds. Omitting TIMEOUT means
+use the default timeout of 5 seconds. Non-positive TIMEOUT means don't hide
+tooltip automatically.
+
+WIDTH, if non-nil, specifies the width of filling each paragraph.
+
+If FRAME-COORDINATES is omitted or nil, automatically obtain the absolute
+coordinates of the top left corner of frame which WINDOW is on. Here,
+`top left corner of frame' represents the origin of `window-pixel-edges'
+and its coordinates are essential for calculating the absolute coordinates
+of the tooltip. If a cons cell like (LEFT . TOP), specifies the frame
+absolute location and makes the calculation slightly faster, but can be
+used only when it's clear that frame is in the specified position. Users
+can get the latest values of frame coordinates for using in the next call
+by referring the variable `pos-tip-saved-frame-coordinates' just after
+calling this function. Otherwise, FRAME-COORDINATES `relative' means use
+the pixel coordinates relative to the top left corner of the frame for
+displaying the tooltip. This is the same effect as
+`pos-tip-use-relative-coordinates' is non-nil.
+
+DX specifies horizontal offset in pixel.
+
+DY specifies vertical offset in pixel. This makes the calculations done
+without considering the height of object at POS, so the object might be
+hidden by the tooltip.
+
+See also `pos-tip-show-no-propertize'."
+  (unless window
+    (setq window (selected-window)))
+  (let* ((frame (window-frame window))
+	 (max-width (pos-tip-x-display-width frame))
+	 (max-height (pos-tip-x-display-height frame))
+	 (w-h (pos-tip-string-width-height string))
+         ;; (fg (pos-tip-compute-foreground-color tip-color))
+         ;; (bg (pos-tip-compute-background-color tip-color))
+         ;; (frame-font (find-font (font-spec :name (frame-parameter frame 'font))))
+         ;; (tip-face-attrs (list :font frame-font :foreground fg :background bg))
+         )
+    (cond
+     ((and width
+	   (> (car w-h) width))
+      (setq string (pos-tip-fill-string string width nil 'none nil max-height)
+	    w-h (pos-tip-string-width-height string)))
+     ((or (> (car w-h) max-width)
+	  (> (cdr w-h) max-height))
+      (setq string (pos-tip-truncate-string string max-width max-height)
+	    w-h (pos-tip-string-width-height string))))
+    (pos-tip-show-no-propertize
+     string
+     ;; (propertize string 'face tip-face-attrs)
+     tip-color pos window timeout
+     (pos-tip-tooltip-width (car w-h) (frame-char-width frame))
+     (pos-tip-tooltip-height (cdr w-h) (frame-char-height frame) frame)
+     frame-coordinates dx dy)))
 
 (defsubst nvp-hap--skip-footers ()
   (beginning-of-line)
@@ -192,7 +266,7 @@
   (-when-let (doc (nvp-hap--docstring thing arg))
     (let ((x-gtk-use-system-tooltips nil))
       (unless (x-hide-tip)
-        (pos-tip-show doc nil nil nil nvp-hap-popup-timeout)))))
+        (nvp-pos-tip-show doc nil nil nil nvp-hap-popup-timeout)))))
 
 
 ;; -------------------------------------------------------------------

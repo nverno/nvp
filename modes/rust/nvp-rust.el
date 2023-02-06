833806cf700d8f71726c9d1b0f17fccbd1606d12
nverno <noah.v.peart@gmail.com>
2023-02-06 10:04:14 -0800

    remove old rust stuff

--- modes/rust/nvp-rust.el
+++ modes/rust/nvp-rust.el
@@ -1,9 +1,15 @@
 ;;; nvp-rust.el --- rustls -*- lexical-binding: t; -*-
 ;;; Commentary:
+;; TODO:
+;; - macroexp: rustc --pretty expanded, or rustc --pretty expanded,hygiene
+;;   https://doc.rust-lang.org/book/macros.html#debugging-macro-code
+;; - deps
 ;;; Code:
 (eval-when-compile (require 'nvp-macro))
 ;; (require 'rust-mode)
-(nvp:decls :v (toml-mode-map))
+(require 'rustic nil t)
+(nvp:decls :v (toml-mode-map) :f (rustic-cargo-current-test))
+(nvp:auto 'rustic-cargo rustic-cargo--get-test-target)
 
 (cl-defmethod nvp-newline-dwim-comment
   (syntax arg &context (major-mode rust-mode))
@@ -13,69 +19,12 @@
   (syntax arg &context (major-mode rustic-mode))
   (nvp-newline-dwim--comment syntax arg " * "))
 
-;; TODO:
-;; - macroexp: rustc --pretty expanded, or rustc --pretty expanded,hygiene
-;;   https://doc.rust-lang.org/book/macros.html#debugging-macro-code
-;; - deps
-
-;; (defvar nvp-rust-src-repo "https://github.com/rust-lang/rust")
-;; (defvar nvp-rust-rust-home (or (getenv "RUST_SRC_PATH")
-;;                                (expand-file-name "rust/src" (getenv "DEVEL"))))
-
-;;; Tag
-
-;; clone/update source repo, tag it, set RUST_SRC_PATH
-;; (defun nvp-rust-tag-source (arg &optional noretry)
-;;   (interactive "P")
-;;   (let ((have-src (file-exists-p nvp-rust-rust-home))
-;;         (tags (expand-file-name "TAGS" nvp-rust-rust-home)))
-;;     (if (and (not noretry)
-;;              (or (not have-src) (eq '(16) arg)))
-;;         ;; clone/pull first
-;;         (set-process-sentinel
-;;          (apply 'start-process "nvp-rust" "*nvp-install*" "git"
-;;                 (delq nil
-;;                       `(,@(if (not have-src)
-;;                               `("clone" ,nvp-rust-src-repo)
-;;                             '("pull" "origin" "master"
-;;                               "--allow-unrelated-histories"))
-;;                         "--depth=10"
-;;                         ,(when (not have-src)
-;;                            (directory-file-name
-;;                             (file-name-directory nvp-rust-rust-home))))))
-;;          (lambda (p _m)
-;;            (when (zerop (process-exit-status p))
-;;              ;; set RUST_SRC_PATH
-;;              (nvp:with-w32
-;;                (nvp-rust-w32-setenv! "RUST_SRC_PATH"
-;;                                        nvp-rust-rust-home))
-;;              (nvp-rust-tag-source nil t))))
-;;       ;; tag source / load tags table when finished
-;;       (when have-src
-;;         (if (and (not arg) (file-exists-p tags))
-;;             (visit-tags-table tags)
-;;           (user-error "TODO: tag rust source")
-;;           ;; (tag-utils-tag-dir nvp-rust-rust-home)
-;;           )))))
-
-;; -------------------------------------------------------------------
-;;; Cargo
-
-(defun nvp-rust-cargo-help ()
+(defun nvp-rust-current-test-dwim ()
   (interactive)
-  (browse-url "http://doc.crates.io/guide.html"))
-
-(defun nvp-rust-locate-cargo ()
-  (or (and buffer-file-name
-           (expand-file-name
-            "Cargo.toml"
-            (locate-dominating-file buffer-file-name "Cargo.toml")))
-      (user-error "No Cargo.toml found")))
-
-(defun nvp-rust-open-cargo ()
-  (interactive)
-  (when-let ((cargo (nvp-rust-locate-cargo)))
-    (find-file cargo)))
+  (if (rustic-cargo--get-test-target)
+      (rustic-cargo-current-test)
+    (when (fboundp 'lsp-rust-analyzer-related-tests)
+      (call-interactively #'lsp-rust-analyzer-related-tests))))
 
 ;; -------------------------------------------------------------------
 ;;; Insert / Toggle
@@ -111,43 +60,35 @@
 ;;       (insert "mut ")
 ;;       (message "mutability on"))))
 
-;; convert vector expression at point to slice
-;; foo -> &foo[..]
-(defun nvp-rust-slice ()
-  (interactive)
-  (insert "&")
-  (forward-symbol 1)
-  (insert "[..]"))
-
 ;;; Enums / Struct
 
-(eval-when-compile
-  ;; do body business at item location
-  (defmacro nvp-rust-at-definition-of (item &rest body)
-    (declare (indent defun))
-    `(save-excursion
-       (with-current-buffer (find-file-noselect
-                             (get-text-property 0 'file ,item))
-         (goto-char (point-min))
-         (forward-line (1- (get-text-property 0 'line ,item)))
-         (forward-char (get-text-property 0 'col ,item))
-         ,@body)))
+;; (eval-when-compile
+;;   ;; do body business at item location
+;;   (defmacro nvp-rust-at-definition-of (item &rest body)
+;;     (declare (indent defun))
+;;     `(save-excursion
+;;        (with-current-buffer (find-file-noselect
+;;                              (get-text-property 0 'file ,item))
+;;          (goto-char (point-min))
+;;          (forward-line (1- (get-text-property 0 'line ,item)))
+;;          (forward-char (get-text-property 0 'col ,item))
+;;          ,@body)))
 
-  ;; collect stuff between beginning/end of rust def at point
-  (defmacro nvp-rust-collect-fields (regexp)
-    (declare (indent defun))
-    `(save-excursion
-       (rust-beginning-of-defun)
-       (let ((end (save-excursion
-                    (rust-end-of-defun)
-                    (point)))
-             (case-fold-search)
-             opts)
-         (while (< (point) end)
-           (and (looking-at ,regexp)
-                (push (match-string-no-properties 1) opts))
-           (forward-line 1))
-         opts))))
+;;   ;; collect stuff between beginning/end of rust def at point
+;;   (defmacro nvp-rust-collect-fields (regexp)
+;;     (declare (indent defun))
+;;     `(save-excursion
+;;        (rust-beginning-of-defun)
+;;        (let ((end (save-excursion
+;;                     (rust-end-of-defun)
+;;                     (point)))
+;;              (case-fold-search)
+;;              opts)
+;;          (while (< (point) end)
+;;            (and (looking-at ,regexp)
+;;                 (push (match-string-no-properties 1) opts))
+;;            (forward-line 1))
+;;          opts))))
 
 ;; find type in let expression
 (defun nvp-rust-let-type ()
@@ -212,44 +153,6 @@
 ;;   (let ((_meta (racer--call-at-point "find-definition")))
 ;;     ;; FIXME:
 ;;     ))
-
-;; -------------------------------------------------------------------
-;;; Minor Mode
-
-(defvar nvp-rust-menu
-  '("RU"
-    ;; ["Tag Source" nvp-rust-tag-source
-    ;;  :help "C-u C-u update source, C-u retag"]
-    ["Open Cargo.toml" nvp-rust-open-cargo]
-    "--"
-    ;; ["Install/Upgrade" nvp-rust-install
-    ;;  :help "Choco upgrade or install with prefix."]
-    ))
-
-(defvar nvp-rust-mode-map
-  (let ((km (make-sparse-keymap)))
-    (easy-menu-define nil km nil nvp-rust-menu)
-    ;; (define-key km (kbd "<f2> m t") #'nvp-rust-tag-source)
-    (define-key km (kbd "<f2> m .") #'xref-find-definitions)
-    ;; (define-key km (kbd "<f2> M-p") #'nvp-rust-toggle-pub)
-    ;; (define-key km (kbd "<f2> M-m") #'nvp-rust-toggle-mut)
-    (define-key km (kbd "<f2> M-v") #'nvp-rust-slice)
-    (define-key km (kbd "<f2> m j c") #'nvp-rust-open-cargo)
-    km))
-
-;;;###autoload
-(define-minor-mode nvp-rust-mode
-  "Rust utilities."
-  :keymap nvp-rust-mode-map
-  :lighter " RU"
-  ;; (nvp-rust-tag-source nil)
-  )
-
-(defun nvp-rust-toml-hook ()
-  (define-key toml-mode-map (kbd "M-?") #'nvp-rust-cargo-help))
-
-;;;###autoload (add-hook 'toml-mode-hook 'nvp-rust-toml-hook)
-;;;###autoload (add-hook 'rust-mode-hook 'nvp-rust-mode)
 
 (provide 'nvp-rust)
 ;;; nvp-rust.el ends here

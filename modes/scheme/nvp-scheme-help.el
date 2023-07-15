baa532fee37bd959b44233fb402de93daa2beef4
nverno <noah.v.peart@gmail.com>
2023-07-14 17:51:12 -0700

    convert scheme help-at-point to hap backend

--- modes/scheme/nvp-scheme-help.el
+++ modes/scheme/nvp-scheme-help.el
@@ -3,11 +3,24 @@
 ;;; Commentary:
 ;;; Code:
 (eval-when-compile (require 'nvp-macro))
-(require 'geiser)
-(require 'geiser-doc)
+(require 'geiser nil t)
+(require 'geiser-doc nil t)
 (nvp:decls :v (geiser-impl--implementation)
-           :f (geiser-eval--get-module))
-(nvp:auto "pos-tip" 'pos-tip-show)
+           :f (geiser-eval--get-module
+               geiser-doc--external-help geiser-doc--module geiser-doc--get-docstring
+               geiser-doc-symbol))
+(nvp:auto "nvp-hap" nvp-hap-thing-at-point)
+
+;;;###autoload
+(defun nvp-scheme-help-company-show-doc ()
+  "Show the doc for current company selection."
+  (interactive)
+  (let ((selected (nth company-selection company-candidates)))
+    (geiser-doc--external-help geiser-impl--implementation
+                               selected (geiser-eval--get-module))))
+
+;; -------------------------------------------------------------------
+;;; Help at point 
 
 ;; doc for symbol at point
 (defun nvp-scheme-help--doc-string (sym &optional module impl)
@@ -19,19 +32,28 @@
         (format "No documentation available for '%s'" sym)
       (substring (cdr (assoc "docstring" ds)) 0 -1))))
 
-(eval-when-compile
-  ;; cycle between links in doc buffer
-  (defmacro nvp-scheme-help--next-link ()
-    `(let ((pnt (next-single-char-property-change (point) 'category)))
-       (while (not (or (eq pnt (point-max))
-                       (get-text-property pnt 'category)))
-         (setq pnt (next-single-char-property-change pnt 'category)))
-       pnt)))
+;;;###autoload
+(defun nvp-hap-scheme (command &optional arg &rest _args)
+  (cl-case command
+    (thingatpt (intern (nvp-hap-thing-at-point (car arg))))
+    (doc-string (nvp-scheme-help--doc-string arg))
+    (doc-buffer
+     (save-window-excursion
+       (geiser-doc-symbol arg)
+       (list (current-buffer) (point-min))))))
+
+;; -------------------------------------------------------------------
+;;; FIXME: unused - scheme help minor mode
 
 ;;; Doc-mode, not used anymore -- not sure wy I needed it
 (defun nvp-scheme-help-next-link ()
   (interactive)
-  (let ((plink (nvp-scheme-help--next-link)))
+  (let ((plink
+         (let ((pnt (next-single-char-property-change (point) 'category)))
+           (while (not (or (eq pnt (point-max))
+                           (get-text-property pnt 'category)))
+             (setq pnt (next-single-char-property-change pnt 'category)))
+           pnt)))
     (if (not (eq plink (point-max)))
         (goto-char plink)
       (unless (eq plink (point-min))
@@ -47,34 +69,6 @@
   :lighter " Doc"
   :keymap 'nvp-scheme-help-doc-mode-map
   (view-mode-enter))
-
-;; -------------------------------------------------------------------
-;;; Commands
-
-;; help in popup tooltip for thing at point
-;;;###autoload
-(defun nvp-scheme-help-at-point (sym)
-  (interactive (list (symbol-at-point)))
-  (let ((str (nvp-scheme-help--doc-string sym))
-        (sym sym))
-    (nvp:with-toggled-tip str
-      :help-fn #'(lambda ()
-                   (interactive)
-                   (x-hide-tip)
-                   (geiser-doc-symbol sym)
-                   ;; (geiser-company--doc-buffer (symbol-name sym))
-                   ;; (pop-to-buffer "*company-documentation*")
-                   ;; (nvp-scheme-help-doc-mode)
-                   ))))
-
-;;;###autoload
-(defun nvp-scheme-help-company-show-doc ()
-  "Show the doc for current company selection."
-  (interactive)
-  (let ((selected (nth company-selection company-candidates)))
-    (geiser-doc--external-help geiser-impl--implementation
-                               selected (geiser-eval--get-module))))
-
 
 (provide 'nvp-scheme-help)
 ;;; nvp-scheme-help.el ends here

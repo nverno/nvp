58e6eaa992c6038730329c17c57d313cf539f6ce
nverno <noah.v.peart@gmail.com>
2023-07-18 18:29:02 -0700

    add function to compile/run makefile with shell debugging output

--- modes/makefile/nvp-makecheck.el
+++ modes/makefile/nvp-makecheck.el
@@ -1,4 +1,4 @@
-;;; nvp-makecheck.el --- lint makefile -*- lexical-binding: t; -*-
+;;; nvp-makecheck.el --- lint/debug makefile -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 ;; - https://github.com/mrtazz/checkmake
@@ -8,17 +8,34 @@
 (require 'compile)
 (require 'nvp)
 
+(defsubst nvp-makefile-read-targets ()
+  (mapconcat 'identity (nvp-makefile-completing-read (buffer-file-name)) " "))
+
 ;;;###autoload
 (defun nvp-makefile-check (&optional targets)
   "Dry run makefile TARGETS to report undefined variables in compilation buffer."
-  (interactive
-   (list
-    (mapconcat 'identity (nvp-makefile-completing-read (buffer-file-name)) " ")))
+  (interactive (list (nvp-makefile-read-targets)))
   (let ((compilation-error-regexp-alist '(makefile))
         (compilation-error-regexp-alist-alist
          '(makefile "\\([^:]+\\):\\([0-9]+\\)" 1 2)))
     (compilation-start
      (concat "make -n --warn-undefined-variables -f " (buffer-file-name) " " targets))))
+
+(defun nvp-makefile-debug (&optional targets)
+  "Run makefile TARGETS with shell debugging output."
+  (interactive (list (nvp-makefile-read-targets)))
+  (let ((compilation-error-regexp-alist '(makefile))
+        (compilation-error-regexp-alist-alist
+         '(makefile "\\([^:]+\\):\\([0-9]+\\)" 1 2))
+        (cmd (concat
+              "cat <<TARGET | make -f - run-debug
+include " (buffer-file-name) "
+_SHELL := \\$(SHELL)
+SHELL = \\$(warning [\\$@])\\$(_SHELL) -x
+run-debug: " targets "
+TARGET
+")))
+    (compilation-start cmd)))
 
 (provide 'nvp-makecheck)
 ;; Local Variables:

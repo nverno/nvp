dde320033b60dc9937e400a0402288fce85f4138
nverno <noah.v.peart@gmail.com>
2023-08-25 22:23:58 -0700

    add treesit dev mode

--- nvp-treesit.el
+++ nvp-treesit.el
@@ -4,31 +4,61 @@
 ;;; Code:
 (eval-when-compile (require 'nvp-macro))
 (require 'treesit nil t)
-(nvp:decls :f (treesit-auto--build-treesit-source-alist))
+(nvp:decls :p ("treesit-auto"))
+
+;; Setup treesitter sources
+(defun nvp-treesit--setup-sources ()
+  (when (require 'treesit-auto nil t)
+    (unless treesit-language-source-alist
+      (setq treesit-language-source-alist (treesit-auto--build-treesit-source-alist)))))
+
+;; maybe load sources
+(cl-eval-when (load)
+  (nvp-treesit--setup-sources))
 
 ;;;###autoload
 (defun nvp-treesit-install ()
   "Install tree-sitter grammar."
   (interactive)
-  (when (require 'treesit-auto nil t)
-    (unless treesit-language-source-alist
-      (setq treesit-language-source-alist (treesit-auto--build-treesit-source-alist))))
+  (nvp-treesit--setup-sources)
   ;; Notes:
   ;; To remap mode to tree-sitter mode
   ;; (cl-pushnew '(<major-mode> . <treesit-mode>) major-mode-remap-alist :test #'equal)
   ;; May need to restart emacs for shared grammar library to be loaded?
   (call-interactively #'treesit-install-language-grammar))
 
+;; -------------------------------------------------------------------
+;;; Dev Minor Mode
+
+(defun nvp-treesit-validate (lang query)
+  (interactive
+   (list
+    (or (treesit-language-at (point))
+        (completing-read "Language: " (mapcar #'car treesit-language-source-alist)))
+    (read-from-minibuffer "Query: " (sexp-at-point))))
+  (treesit-query-validate lang query))
+
+(defvar-keymap nvp-treesit-minor-mode-map
+  :doc "T/S keymap"
+  "C-c v" #'nvp-treesit-validate
+  "C-M-?" #'treesit-inspect-node-at-point)
+
+(easy-menu-define nvp-treesit-minor-mode-menu nvp-treesit-minor-mode-map
+  "T/S menu."
+  '("T/S"
+    ["Inspect" treesit-inspect-node-at-point t]
+    ["Validate" nvp-treesit-validate t]))
+
 ;;;###autoload
 (define-minor-mode nvp-treesit-minor-mode
   "Treesit development minor mode."
   :lighter " T/S"
-  :keymap nvp-treesit-mode-map
+  :keymap nvp-treesit-minor-mode-map
   ;; XXX: powerline doesn't display `mode-line-misc-info' where
   ;; `treesit-inspect-mode' wants to put the node info
   ;; (powerline-revert)
   (if nvp-treesit-minor-mode
-      (progn
+      (when (treesit-language-at (point))
         (powerline-revert)
         (treesit-inspect-mode)
         (treesit-explore-mode))

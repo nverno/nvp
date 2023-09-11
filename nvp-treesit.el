9a8d097ecb9a4f5005f756bd9870d1ceb94781e0
nverno <noah.v.peart@gmail.com>
2023-09-11 09:05:44 -0700

    show error/missing nodes in tree sitter dev mode

--- nvp-treesit.el
+++ nvp-treesit.el
@@ -28,6 +28,53 @@
   (call-interactively #'treesit-install-language-grammar))
 
 ;; -------------------------------------------------------------------
+;;; Errors / missing nodes
+
+(defface nvp-treesit-error
+  '((((supports :underline (:style wave)))
+     :underline (:style wave :color "Red1"))
+    (t :underline t :inherit error))
+  "Face for tree sitter errors / missing nodes.")
+
+(defvar-local nvp-treesit--errors-query nil
+  "Tree sitter query to match errors and missing identifiers")
+
+(defun nvp-treesit--query-compile ()
+  "Compile error/missing quiery."
+  (when (treesit-available-p)
+    (and-let* ((lang (treesit-language-at (point))))
+      (setq nvp-treesit--errors-query
+            (treesit-query-compile lang '(((identifier) @id (:equal "" @id))
+                                          (ERROR) @err))))))
+
+(defun nvp-treesit--make-error-overlay (beg end)
+  "Make error overlay from BEG to END."
+  (let ((ov (make-overlay beg end)))
+    (overlay-put ov 'face 'nvp-treesit-error)
+    (overlay-put ov 'nvp-treesit t)))
+
+(defun nvp-treesit--update-errors ()
+  "Update error/missing node overlays."
+  (when nvp-treesit--errors-query
+    (and-let* ((lang (treesit-language-at (point))))
+      (if-let ((ranges
+                (treesit-query-range
+                 lang nvp-treesit--errors-query (point-min) (point-max))))
+          (prog1 t
+            (dolist (range ranges)
+              (nvp-treesit--make-error-overlay (car range) (1+ (cdr range)))))
+        (remove-overlays (point-min) (point-max) 'nvp-treesit t)
+        nil))))
+
+(defun nvp-treesit-toggle-errors ()
+  "Toggle tree sitter error overlays in buffer."
+  (interactive)
+  (when nvp-treesit--errors-query
+    (if (memq #'nvp-treesit--update-errors after-change-functions)
+        (remove-hook 'after-change-functions #'nvp-treesit--update-errors t)
+      (add-hook 'after-change-functions #'nvp-treesit--update-errors nil t))))
+
+;; -------------------------------------------------------------------
 ;;; Dev Minor Mode
 
 (defun nvp-treesit-validate (lang query)
@@ -40,8 +87,9 @@
 
 (defvar-keymap nvp-treesit-minor-mode-map
   :doc "T/S keymap"
-  "C-c v" #'nvp-treesit-validate
-  "C-M-?" #'treesit-inspect-node-at-point)
+  "C-c C-v" #'nvp-treesit-validate
+  "C-c C-s" #'nvp-treesit-toggle-errors
+  "C-M-?"   #'treesit-inspect-node-at-point)
 
 (easy-menu-define nvp-treesit-minor-mode-menu nvp-treesit-minor-mode-map
   "T/S menu."
@@ -59,9 +107,11 @@
   ;; (powerline-revert)
   (if nvp-treesit-minor-mode
       (when (treesit-language-at (point))
+        (add-hook 'after-change-functions #'nvp-treesit--update-errors nil t)
         (powerline-revert)
         (treesit-inspect-mode)
         (treesit-explore-mode))
+    (remove-hook 'after-change-functions #'nvp-treesit--update-errors t)
     (treesit-inspect-mode -1)
     (treesit-explore-mode -1)
     (nvp-theme-switch)))

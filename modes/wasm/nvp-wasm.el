e05093acb02a836eeb0d4afb3004b427724fb174
nverno <noah.v.peart@gmail.com>
2023-09-19 18:17:57 -0700

    wrap/splice wat instructions

--- modes/wasm/nvp-wasm.el
+++ modes/wasm/nvp-wasm.el
@@ -46,6 +46,37 @@ With prefix SAVE, dont cleanup wasm/wat files."
           (error (cleanup-wasm nil 'force)
                  (error (error-message-string err))))))))
 
+(eval-when-compile
+  (cl-defmacro nvp:with-treesit-sexp (node &rest body
+                                           &key bounds (type "instr")
+                                           &allow-other-keys)
+    (declare (indent defun))
+    (nvp:skip-keywords body)
+    `(when-let (it (treesit-parent-until ,node (lambda (n) (treesit-node-match-p n ,type))))
+       (,@(if bounds `(let ((beg (treesit-node-start it))
+                            (end (treesit-node-end it))))
+            '(progn))
+        ,@body))))
+
+(defun nvp-wat-wrap-instruction (node)
+  "Wrap instruction NODE with parens."
+  (interactive (list (treesit-node-at (point))))
+  (nvp:with-treesit-sexp node :bounds t
+    (save-excursion
+      (goto-char end)
+      (insert ")")
+      (goto-char beg)
+      (insert "("))))
+
+(defun nvp-wat-splice (node)
+  "Splice parens surround expression NODE."
+  (interactive (list (treesit-node-at (point))))
+  (nvp:with-treesit-sexp node :bounds t :type "expr\\'"
+    (save-excursion (goto-char (1- end))
+                    (delete-char 1)
+                    (goto-char beg)
+                    (delete-char 1))))
+
 (provide 'nvp-wasm)
 ;; Local Variables:
 ;; coding: utf-8

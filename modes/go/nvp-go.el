4319bede1d46b83385cdf8d1dbc9ec4fe10bc8b4
nverno <noah.v.peart@gmail.com>
2023-10-25 02:09:22 -0700

    squash

--- modes/go/nvp-go.el
+++ modes/go/nvp-go.el
@@ -31,14 +31,6 @@
                 (gorepl--run-gore '("-autoimport"))
                 (get-buffer-process (current-buffer)))))))
 
-;;; Help
-
-;; temporarily capture `package.function' as symbol
-(defvar nvp-go-thingatpt-syntax-table
-  (let ((st (make-syntax-table)))
-    (modify-syntax-entry ?. "_" st)
-    st))
-
 ;;; Yas
 (nvp:decl yas-text nvp-yas-split-args)
 (defun nvp-go-params (&optional str types join)

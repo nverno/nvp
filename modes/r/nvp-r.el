bf946181bb08cad764f643d94ea9d6f56abc84d0
Noah Peart <noah.v.peart@gmail.com>
2024-03-27 23:37:23 -0700

    add nvp-repl for R/ess

--- modes/r/nvp-r.el
+++ modes/r/nvp-r.el
@@ -20,16 +20,40 @@
           (insert ?\n it))
       (newline-and-indent arg))))
 
-;;; TODO: REPL
-;; (with-eval-after-load 'nvp-repl
-;;   (nvp-repl-add '(inferior-ess-r-mode)
-;;     :name 'R
-;;     :modes '(ess-r-mode r-ts-mode)
-;;     :init
-;;     ;; :help-cmd
-;;     ;; :pwd-cmd
-;;     ;; :cd-cmd
-;;     ))
+;;; REPL
+(defun nvp-r--repl-init (&optional _prefix)
+  (interactive "P")
+  (save-window-excursion
+    (let ((ess-dialect "R"))
+      (ess-force-buffer-current nil 'force))))
+
+;; Dont error if `add-log-current-defun-header-regexp' isnt defined,
+;; eg. in `r-ts-mode'
+(defvar add-log-current-defun-header-regexp)
+(define-advice ess-eval-function (:around (orig &rest args) "add-log-regex")
+  (let ((add-log-current-defun-header-regexp "^\\(.+\\)\\s-+<-[ \t\n]*function"))
+    (apply orig args)))
+
+(with-eval-after-load 'nvp-repl
+  (nvp-repl-add '(ess-r-mode r-ts-mode)
+    :name 'R
+    :modes '(inferior-ess-r-mode)
+    :init #'nvp-r--repl-init
+    :find-fn (lambda () (-some-> ess-local-process-name get-process))
+    :send-string #'ess-send-string
+    :send-region (lambda (start end) (ess-send-region (nvp-repl-process) start end))
+    :send-file #'ess-load-file
+    :send-defun #'ess-eval-function
+    :send-buffer #'ess-eval-buffer
+    :send-line #'ess-eval-line
+    ;; :send-sexp
+    ;; :send-statement
+    ;; :eval-sexp #'ess-eval-paragraph
+    ;; :eval-region #'ess-eval-region
+    :history-file ".Rhistory"
+    :help-cmd '(:no-arg "" :with-arg "help(\"%s\")")
+    :pwd-cmd "getwd()"
+    :cd-cmd "setwd(\"%s\")"))
 
 ;;; Abbrev
 ;; dont expand in comments/strings or in symbols with '.', eg. is.null

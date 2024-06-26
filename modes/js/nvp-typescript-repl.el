d8065cfd28b7dbdb0d650a46a0677b1547584746
Noah Peart <noah.v.peart@gmail.com>
2024-06-26 06:09:01 -0400

    add typescript repl eval command

--- modes/js/nvp-typescript-repl.el
+++ modes/js/nvp-typescript-repl.el
@@ -9,26 +9,6 @@
 (require 'nvp-repl)
 (nvp:decls :p (ts add-node) :v (ts-repl-buffer nvp-typescript-modes) :f (ts-repl-run))
 
-;; (defvar ts-comint-drop-regexp
-;;   "\\(\x1b\\[[0-9]+[GJK]\\|^[ \t]*undefined[\r\n]+\\)"
-;;   "Regex to silence matching output.")
-
-;; FIXME: doesn't handle multi-line echoed input
-;; (defun nvp-typescript-repl--preoutput-filter (string)
-;;   "Filter extra escape sequences from STRING."
-;;   (if (string= "> " string) ""
-;;     (if (string-prefix-p "undefined" string)
-;;         (substring string (length "undefined"))
-;;       string))
-;;   ;; (let ((beg (or comint-last-output-start
-;;   ;;                (point-min-marker)))
-;;   ;;       (end (process-mark (get-buffer-process (current-buffer)))))
-;;   ;;   (save-excursion
-;;   ;;     (goto-char beg)
-;;   ;;     ;; Remove ansi escape sequences used in readline.js
-;;   ;;     (while (re-search-forward ts-comint-drop-regexp end t)
-;;   ;;       (replace-match ""))))
-;;   )
 
 (defvar nvp-typescript--repl-commands
   '("break" "clear" "exit" "help" "save" "load" "editor" "type"))
@@ -41,10 +21,7 @@
     :init #'ts-repl-run
     :find-fn #'ts-repl-process
     :send-region #'ts-repl-send-region
-    ;; :send-string #'ts-send-string
-    ;; :send-sexp #'ts-send-last-sexp
-    ;; :send-buffer #'ts-send-buffer
-    ;; :send-file #'ts-load-file
+    :eval-string #'ts-repl-eval-string
     :commands nvp-typescript--repl-commands
     :cmd-prefix ?.
     :help-cmd '(:no-arg ".help" :with-arg ".type %s")
@@ -61,17 +38,6 @@
        (concat "console.table(util.inspect.styles);"
                "console.table(util.inspect.defaultOptions)"))
     (shell-command "npx ts-node --showConfig")))
-
-;; (defun nvp-typescript-repl-get-file-mod (filename)
-;;   (concat "import * as "
-;;           (replace-regexp-in-string
-;;            "^[0-9_]+" ""
-;;            (replace-regexp-in-string "-" "_" (file-name-base filename)))
-;;           " from \""
-;;           (file-name-base filename)
-;;           "\"\n"))
-;; ;;; FIXME: remove
-;; (advice-add 'ts-comint--get-load-file-cmd :override #'nvp-typescript-repl-get-file-mod)
 
 (provide 'nvp-typescript-repl)
 ;; Local Variables:

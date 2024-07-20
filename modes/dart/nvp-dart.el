c1aeba2d83c66ff5098c388d35b8574991264eb7
Noah Peart <noah.v.peart@gmail.com>
2024-07-20 00:44:14 -0700

    fix: push button dwim

---
+++ modes/dart/nvp-dart.el
@@ -0,0 +1,13 @@
+;;; nvp-dart.el --- Dart -*- lexical-binding: t; -*-
+;;
+;;; Commentary:
+;;; Code:
+(eval-when-compile (require 'nvp-macro))
+(nvp:decls)
+
+(provide 'nvp-dart)
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
+;;; nvp-dart.el ends here

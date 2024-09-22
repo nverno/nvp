ea3da3c902f28d75563a03cc29b9a7239ba49d17
Noah Peart <noah.v.peart@gmail.com>
2024-09-22 07:14:41 -0700

    separate trace-mode/tracing to package

--- nvp-trace.el
+++ nvp-trace.el
@@ -6,29 +6,16 @@
 (require 'trace)
 (require 'transient)
 
-(nvp:decls :f (flatten-tree) :v (tracing-minor-mode))
+(nvp:decls :p (trace-mode tracing) :f (flatten-tree) :v (tracing-minor-mode))
 
 (autoload 'find-library-name "find-func")
 (autoload 'nvp-elisp-matching-forms "nvp-elisp" )
-(autoload 's-split "s")
 (autoload 'trace-tree-disable "trace-tree")
+(autoload 'tracing-update-mode-line "tracing")
 
-
-(defun nvp-trace-buffer (&optional and-go create)
-  (let ((buf (if create
-                 (get-buffer-create trace-buffer)
-               (get-buffer trace-buffer))))
-    (or buf (user-error "No trace-buffer"))
-    (with-current-buffer buf
-      (when (eq major-mode 'fundamental-mode)
-        (trace-output-mode))
-      (if and-go
-          (pop-to-buffer (current-buffer))
-        (display-buffer (current-buffer))))))
-
-(defun nvp-trace-active-p ()
-  (and (boundp 'trace-buffer)
-       (buffer-live-p (get-buffer trace-buffer))))
+(defvar tracing--batch nil)
+(when (fboundp 'tracing-enable)
+  (tracing-enable))
 
 
 ;;; Menu
@@ -39,7 +26,12 @@
   :reader (lambda (&rest _) (not inhibit-trace))
   :set-value (lambda (sym val)
                (set sym val)
-               (tracing-update-mode-line)))
+               (when (fboundp 'tracing-update-mode-line)
+                 (tracing-update-mode-line))))
+
+(defun nvp-trace-active-p ()
+  (and (boundp 'trace-buffer)
+       (buffer-live-p (get-buffer trace-buffer))))
 
 ;;;###autoload(autoload 'nvp-trace-menu "nvp-trace" nil t)
 (transient-define-prefix nvp-trace-menu ()
@@ -57,146 +49,14 @@
     ("l" "Library" nvp-trace-library)
     ("h" "Hooks" nvp-trace-hooks)]
    ["Results" :if nvp-trace-active-p
-    ("j" "Display" nvp-trace-display-results)
-    ("k" "Clear" nvp-trace-clear)]
+    ("j" "Display" trace-mode-display-results)
+    ("k" "Clear" trace-mode-clear)]
    ["Tree mode" :if (lambda () (featurep 'trace-tree))
     ("tt" "Enable" trace-tree-enable)
     ("tq" "Disable" trace-tree-disable)]])
 
 
 ;; -------------------------------------------------------------------
-;;; Tracing Minor Mode
-
-(defvar tracing-mode-line-prefix " Tr")
-(defvar tracing--mode-line-string tracing-mode-line-prefix)
-(defvar tracing--current nil "Active trace names")
-(defvar tracing--batch nil "Non-nil when doing batch action.")
-
-(defun tracing-mode-line-toggle-inhibit (event)
-  "Toggle `inhibit-trace' from the mode-line."
-  (interactive "e")
-  (with-selected-window (posn-window (event-start event))
-    (setq inhibit-trace (not inhibit-trace))
-    (force-mode-line-update t)))
-
-(defun tracing--mode-line ()
-  "Report current trace in the modeline."
-  (let* ((face)
-         (suffix (if inhibit-trace
-                     (progn (setq face 'compilation-error) "off")
-                   (let ((cnt (length tracing--current)))
-                     (when (> cnt 0)
-                       (setq face '(bold compilation-mode-line-exit)))
-                     (number-to-string cnt)))))
-    (concat
-     tracing-mode-line-prefix ":"
-     (apply #'propertize suffix
-            ;; Fix(09/16/24): minor mode help-echo/local-map make this
-            ;; impossible to click, at least when using powerline
-            'help-echo (apply #'format "%s\nmouse-1: %s tracing"
-                              (if inhibit-trace
-                                  (list "Tracing is inhibited" "Enable")
-                                (list "Tracing is active" "Inhibit")))
-            'local-map (make-mode-line-mouse-map
-                        'mouse-1 #'tracing-mode-line-toggle-inhibit)
-            (and face (list 'face face))))))
-
-(defun tracing-update-mode-line ()
-  "Update active trace mode-line."
-  (setq tracing--mode-line-string (tracing--mode-line))
-  (when tracing-minor-mode
-    (force-mode-line-update t)))
-
-(defun nvp-trace-add (funcs &optional _type remove)
-  "Track FUNCS and maybe enable/disable `tracing-minor-mode'.
-If REMOVE is non-nil, remove FUNCS from tracking."
-  (setq tracing--current
-        (if remove
-            (seq-remove (lambda (e) (memq e funcs)) tracing--current)
-          (seq-uniq (append funcs tracing--current))))
-  (cond ((zerop (length tracing--current))
-         (tracing-minor-mode -1))
-        (tracing-minor-mode
-         (tracing-update-mode-line))
-        (t (tracing-minor-mode 1))))
-
-(defun nvp-trace-clear ()
-  "Clear trace results buffer."
-  (interactive)
-  (when-let ((buf (get-buffer trace-buffer)))
-    (and (buffer-live-p buf)
-         (with-current-buffer buf
-           (let ((inhibit-read-only t))
-             (erase-buffer))))))
-
-(defun tracing-list-untrace ()
-  "Untrace function in list."
-  (interactive)
-  (when-let ((args (get-text-property (point) 'trace-args)))
-    (apply #'untrace-function args)
-    (revert-buffer nil t t)))
-
-(defvar-keymap trace-active-list-keymap
-  :doc "Keymap on links in list of traced functions."
-  :parent button-map
-  "u" #'tracing-list-untrace)
-
-(defun nvp-trace-list ()
-  "List functions currently traced."
-  (interactive nil tracing-minor-mode)
-  (unless tracing-minor-mode
-    (user-error "No active trace"))
-  (help-setup-xref (list #'nvp-trace-list)
-		   (called-interactively-p 'interactive))
-  (with-help-window (get-buffer-create "*Traced Functions*")
-    (insert (propertize (format "%s" "Traced functions") 'face 'outline-1))
-    (insert "\n\n")
-    (dolist (fn tracing--current)
-      (insert-text-button
-       (symbol-name fn)
-       'face 'button
-       'keymap trace-active-list-keymap
-       'trace-args (list fn)
-       'action (lambda (_) (describe-function fn))
-       'follow-link t
-       'help-echo "mouse-1, RET: describe function")
-      (insert "\n"))))
-
-(defvar-keymap tracing-minor-mode-map
-  "<f2> D j" #'nvp-trace-display-results
-  "<f2> D q" #'untrace-all
-  "<f2> D l" #'nvp-trace-list)
-
-;;;###autoload
-(define-minor-mode tracing-minor-mode
-  "Minor mode active during tracing."
-  :lighter (:eval tracing--mode-line-string)
-  :keymap tracing-minor-mode-map
-  :global t
-  :interactive nil
-  :group 'trace
-  (if tracing-minor-mode
-      (tracing-update-mode-line)
-    (setq tracing--current nil)))
-
-
-;;; Advices
-
-;;;###autoload
-(defun trace-function-internal@tracing-add (func &rest _)
-  (or tracing--batch (nvp-trace-add (list func))))
-
-;;;###autoload(advice-add 'trace-function-internal :after #'trace-function-internal@tracing-add '((name . "tracing-add")))
-
-(define-advice untrace-function (:after (func) "tracing-remove")
-  (or tracing--batch (nvp-trace-add (list func) nil t)))
-
-(define-advice untrace-all (:around (orig) "tracing-remove-all")
-  (let ((tracing--batch t))
-    (funcall orig)
-    (tracing-minor-mode -1)))
-
-
 ;;; Groups
 
 (defvar nvp-trace-defun-forms
@@ -235,10 +95,10 @@ If REMOVE is non-nil, remove FUNCS from tracking."
         (tracing--batch t))
     (dolist (fn funcs)
       (funcall trace-fn fn))
-    (nvp-trace-add funcs)))
+    (tracing-add funcs)))
 
 (defun nvp-untrace-group (funcs)
-  "Untrace functions in GROUP."
+  "Untrace functions in FUNCS."
   (interactive
    (list (apply #'append
                 (--map
@@ -247,13 +107,7 @@ If REMOVE is non-nil, remove FUNCS from tracking."
   (let ((tracing--batch t))
     (dolist (fn funcs)
       (untrace-function fn))
-    (nvp-trace-add funcs nil t)))
-
-;;;###autoload
-(defun nvp-trace-display-results (&optional and-go)
-  "Show the trace results and maybe enable `trace-output-mode'."
-  (interactive "P")
-  (nvp-trace-buffer and-go))
+    (tracing-add funcs nil t)))
 
 ;;;###autoload
 (defun nvp-trace-library (library &optional macros filter)
@@ -275,7 +129,7 @@ With \\[universal-argument] \\[universal-argument] prompt for filter."
       (setq forms (--filter (string-match-p filter (symbol-name it)) forms)))
     (dolist (fn forms)
       (trace-function-background fn))
-    (nvp-trace-add forms 'library)
+    (tracing-add forms 'library)
     (message "tracing %d funs from %s: %S" (length forms) library forms)))
 
 ;;;###autoload
@@ -296,130 +150,9 @@ With \\[universal-argument] \\[universal-argument] prompt for filter."
           (tracing--batch t))
       (dolist (fn forms)
         (trace-function-background fn))
-      (nvp-trace-add forms 'hooks)
+      (tracing-add forms 'hooks)
       (message "tracing hooks: %S" forms))))
 
-
-;; -------------------------------------------------------------------
-;;; Trace Output Mode
-
-;; Skip over oen trace level. ARG is +1 or -1 for direction
-(defun trace-output--forward-sexp-1 (arg)
-  (let* ((cur-arrow "->")
-         (level
-          (progn (beginning-of-line)
-                 (and (looking-at "\\(\\(?:| \\)*[0-9]+\\) \\([<>-]+\\)")
-                      (prog1 (match-string 1)
-                        (setq cur-arrow (match-string 2))))))
-         (end-re (format "^%s %s" level (if (string= cur-arrow "<-") "->" "<-"))))
-    (when level
-      (if (or (and (string= "->" cur-arrow) (< arg 0))
-              (and (string= "<-" cur-arrow) (> arg 0)))
-          (zerop (forward-line arg))
-        (catch 'term
-          (progn (while (not (looking-at-p end-re))
-                   (unless (zerop (forward-line arg))
-                     (throw 'term nil)))
-                 t))))))
-
-;; Necessary for hideshow to hide levels properly
-(defun trace-output-forward-sexp (&optional arg)
-  "Function for `forward-sexp-function'.
-ARG comes from `forward-sexp', which see."
-  (or arg (setq arg 1))
-  (let ((cnt (abs arg))
-        (sign (if (> arg 0) 1 -1)))
-    (catch 'term
-      (while (> cnt 0)
-        (beginning-of-line)
-        (when (looking-at-p (regexp-quote trace-separator))
-          (or (zerop (forward-line sign))
-              (throw 'term nil)))
-        (trace-output--forward-sexp-1 sign)
-        (throw 'term nil)
-        (cl-decf cnt)))
-    cnt))
-
-(defvar hs-special-modes-alist)
-(with-eval-after-load 'hideshow
-  (unless (assoc 'trace-output-mode hs-special-modes-alist)
-    (push '(trace-output-mode
-            "^\\(?:| \\)*[0-9]+ ->"
-            "^\\(?:| \\)*[0-9]+ <-")
-          hs-special-modes-alist)))
-
-(defun trace-output-hl-line-range ()
-  "Return the bounds of the call at point for `hl-line-range-function'."
-  (save-excursion
-    (beginning-of-line)
-    (if (looking-at "^\\(?:| \\)*[0-9]+ \\(->\\|<-\\)")
-        (let ((forward-p (equal "->" (match-string 1))))
-          (cons (if forward-p (point) (line-beginning-position 2))
-                (progn
-                  (funcall (if forward-p #'forward-sexp #'backward-sexp))
-                  (if forward-p (line-beginning-position 2) (point)))))
-      (cons (point) (line-beginning-position 2)))))
-
-(defun trace-output-untrace ()
-  "Untrace traced function on current line."
-  (interactive)
-  (save-excursion
-    (beginning-of-line)
-    (when (looking-at "^\\(?:| \\)*[0-9]+ \\(?:->\\|<-\\) (?\\([^ ]+\\)")
-      (let ((fn (intern (match-string 1))))
-        (when (and fn (y-or-n-p (format "Untrace %S? " fn)))
-          (message "untracing %S" fn)
-          (untrace-function fn)
-          (setq tracing--current (delq fn tracing--current)))))))
-
-(defvar trace-output-mode-keywords
-  `((,(concat "^" (string-chop-newline trace-separator))
-     . font-lock-comment-face)
-    (,(rx bow (or "nil" "t") eow) . font-lock-constant-face)
-    ("^\\(\\(?:| \\)+\\)?\\([0-9]+\\) \\(->\\) (\\([^ ]+\\)"
-     (1 font-lock-comment-face nil t)
-     (2 '(:inherit font-lock-number-face :weight bold))
-     (3 '(:inherit font-lock-keyword-face :weight bold))
-     (4 font-lock-function-name-face))
-    ("^\\(\\(?:| \\)+\\)?\\([0-9]+\\) \\(<-\\) \\([^:]+\\)\\(:\\) \\([^\n]+\\)"
-     (1 font-lock-comment-face nil t)
-     (2 'font-lock-number-face)
-     (3 'font-lock-operator-face)
-     (4 'font-lock-property-use-face)
-     (5 'font-lock-escape-face)
-     (6 'font-lock-string-face)))
-  "Font-locking for `trace-output-mode'.")
-
-(defvar-keymap trace-output-mode-map
-  :parent special-mode-map
-  "j" #'next-line
-  "k" #'previous-line
-  "h" #'backward-char
-  "l" #'forward-char
-  "u" #'trace-output-untrace
-  "f" #'forward-sexp
-  "b" #'backward-sexp
-  "n" #'forward-paragraph
-  "p" #'backward-paragraph)
-
-;;;###autoload
-(define-derived-mode trace-output-mode special-mode "Trace"
-  "Major mode for displaying trace results.
-
-\\{trace-output-mode-map}"
-  :abbrev-table nil
-  :syntax-table emacs-lisp-mode-syntax-table
-  (setq buffer-read-only nil)
-  (let ((sep (regexp-quote (string-chop-newline trace-separator))))
-    ;; Variables to set for hideshow to work
-    (setq-local paragraph-separate sep)
-    (setq-local comment-start sep))
-  (setq-local comment-end "")
-  (setq-local hs-hide-comments-when-hiding-all nil)
-  (setq-local forward-sexp-function #'trace-output-forward-sexp)
-  (setq-local font-lock-defaults (list trace-output-mode-keywords))
-  (setq-local hl-line-range-function #'trace-output-hl-line-range)
-  (hl-line-mode))
 
 (provide 'nvp-trace)
 ;; Local Variables:

b98997ca8f2d9d8d66bde85f4e3725f5d6badf24
Noah Peart <noah.v.peart@gmail.com>
2024-10-29 23:20:17 -0700

    squash

--- nvp-net.el
+++ nvp-net.el
@@ -3,9 +3,9 @@
 ;;; Commentary:
 ;;; Code:
 (eval-when-compile (require 'nvp-macro))
-(require 'nvp)
 (nvp:decls)
-(nvp:auto "net-utils" net-utils-run-simple)
+
+(autoload 'net-utils-run-simple "net-utils")
 
 (defvar nvp-net-port-history '())
 (defvar nvp-net-options-history '())
@@ -15,16 +15,16 @@
 ;;;###autoload
 (defun nvp-lsof (args)
   (interactive
-   (list
-    (if current-prefix-arg
-        (split-string
-         (read-string
-          "Args: "
-          (mapconcat 'identity nvp-lsof-program-options " ") 'nvp-net-options-history)
-         nil t)
-      (append nvp-lsof-program-options
-              (list (concat ":" (read-string "Port: " nil 'nvp-net-port-history)))))))
-  (net-utils-run-simple (format "*%s*" nvp-lsof-program) nvp-lsof-program args))
+   (list (if current-prefix-arg
+             (split-string
+              (read-string "Args: " (--mapcc nil nvp-lsof-program-options)
+                           'nvp-net-options-history)
+              nil t)
+           `(,@nvp-lsof-program-options
+             ,(concat ":" (read-string "Port: " nil 'nvp-net-port-history))))))
+  (net-utils-run-simple
+   (format "*%s*" nvp-lsof-program)
+   nvp-lsof-program args))
 
 (provide 'nvp-net)
 ;; Local Variables:

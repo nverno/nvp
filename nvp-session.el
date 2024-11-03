284422a33789bc683e8d0d5135daf23d184e2b51
Noah Peart <noah.v.peart@gmail.com>
2024-11-03 15:05:49 -0800

    confirm load/delete sessions from listing

--- nvp-session.el
+++ nvp-session.el
@@ -11,53 +11,64 @@
 ;;; Code:
 
 (eval-when-compile (require 'nvp-macro))
-(require 'nvp)
 (require 'desktop)
 (nvp:decls :f (nvp-buffer-kill-all-buffers))
 
-(eval-when-compile
-  (defsubst nvp-sessions (&optional full)
-    (directory-files (expand-file-name "sessions" nvp/cache) full "^.[^.]"))
-  
-  (defsubst nvp-session--read (prompt)
-    (list (expand-file-name "sessions" nvp/cache)
-          (nvp-completing-read prompt (nvp-sessions))))
+(defsubst nvp--sessions (&optional full)
+  (directory-files (expand-file-name "sessions" nvp/cache) full "^.[^.]"))
 
-  (defmacro with-desktop-vars (dirname filename &rest body)
-    (declare (indent defun))
-    `(cl-letf (((symbol-function #'desktop-owner) #'ignore)
-               (desktop-base-file-name (or ,filename ".emacs.desktop"))
-               (desktop-dirname ,dirname))
-       ,@body)))
+(defsubst nvp--session-read (&optional prompt)
+  "Read session and return list of \\='(filename session-name)."
+  (list (expand-file-name "sessions" nvp/cache)
+        (completing-read (or prompt "Session: ") (nvp--sessions))))
+
+(defmacro with-desktop-vars (dirname filename &rest body)
+  (declare (indent defun))
+  `(cl-letf (((symbol-function #'desktop-owner) #'ignore)
+             (desktop-base-file-name (or ,filename ".emacs.desktop"))
+             (desktop-dirname ,dirname))
+     ,@body))
 
 ;;;###autoload
 (defun nvp-session-save (&optional dirname name)
-  (interactive (nvp-session--read "Save session: "))
+  "Read a session and save it."
+  (interactive (nvp--session-read "Save session: "))
   (with-desktop-vars dirname name
     (desktop-save dirname)))
 
 ;;;###autoload
 (defun nvp-session-load (&optional dirname name)
-  (interactive (nvp-session--read "Load session: "))
+  "Read a session and load it."
+  (interactive (nvp--session-read "Load session: "))
   (call-interactively #'nvp-buffer-kill-all-buffers)
   (with-desktop-vars dirname name
     (desktop-read dirname)))
 
 ;;;###autoload
 (defun nvp-session-delete (&optional dirname name)
-  (interactive (nvp-session--read "Delete session: "))
+  "Read a session and deleete it."
+  (interactive (nvp--session-read "Delete session: "))
   (delete-file (expand-file-name name dirname)))
 
-;; -------------------------------------------------------------------
-;;; List sessions
+
+;;; Session List
+
+(defvar-keymap nvp-sessions-mode-map
+  "d"   #'nvp-sessions-list-delete
+  "o"   #'nvp-sessions-list-load
+  "l"   #'nvp-sessions-list-load
+  "r"   #'nvp-sessions-list-rename
+  "RET" #'nvp-sessions-list-load)
 
 (defun nvp-sessions-tabulate ()
-  (cl-loop for session in (nvp-sessions 'full) collect
+  (cl-loop for session in (nvp--sessions 'full) collect
        (let* ((attr (file-attributes session 'integer))
-              (mod-time (format-time-string "%D" (file-attribute-access-time attr)))
+              (mod-time (format-time-string
+                         "%D" (file-attribute-access-time attr)))
               (access-time (format-time-string
                             "%D" (file-attribute-modification-time attr))))
-         `(,session [,(file-name-nondirectory session) ,access-time ,mod-time]))))
+         `(,session [,(file-name-nondirectory session)
+                     ,access-time ,mod-time]))))
 
 (define-derived-mode nvp-sessions-mode tabulated-list-mode "Sessions"
   "Mode for listing persistent sessions."
@@ -65,39 +76,41 @@
                                ("Last Access" 12 t)
                                ("Modtime" 12 t)])
   (setq tabulated-list-entries #'nvp-sessions-tabulate)
-  (tabulated-list-init-header))
+  (setq tabulated-list-sort-key '("Last Access" . t))
+  (tabulated-list-init-header)
+  (tabulated-list-print))
 
 ;;;###autoload
 (defun nvp-sessions-list ()
+  "List saved sessions."
   (interactive)
   (pop-to-buffer (get-buffer-create "*sessions*"))
   (nvp-sessions-mode)
   (tabulated-list-print))
 
 (defun nvp-sessions-list-delete ()
+  "Delete session at point."
   (interactive)
-  (delete-file (expand-file-name (tabulated-list-get-id)))
+  (let ((session (tabulated-list-get-id)))
+    (when (and session (y-or-n-p "Delete session? "))
+      (delete-file (expand-file-name (tabulated-list-get-id)))))
   (revert-buffer))
 
 (defun nvp-sessions-list-load ()
+  "Kill everything and load session at point."
   (interactive)
   (let ((session (tabulated-list-get-id)))
-    (nvp-session-load (file-name-directory session)
-                      (file-name-nondirectory session))))
+    (when (and session (y-or-n-p "Load session? "))
+      (nvp-session-load (file-name-directory session)
+                        (file-name-nondirectory session)))))
 
 (defun nvp-sessions-list-rename (name)
+  "Rename sesssion at point."
   (interactive "sRename session: ")
   (let ((session (tabulated-list-get-id)))
-    (rename-file session (expand-file-name name (file-name-directory session)))
+    (rename-file session (expand-file-name
+                          name (file-name-directory session)))
     (revert-buffer)))
-
-(nvp:bindings nvp-sessions-mode :now
-  :create t
-  ("d"   . nvp-sessions-list-delete)
-  ("o"   . nvp-sessions-list-load)
-  ("l"   . nvp-sessions-list-load)
-  ("r"   . nvp-sessions-list-rename)
-  ("RET" . nvp-sessions-list-load))
 
 (provide 'nvp-session)
 ;; Local Variables:
